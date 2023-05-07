8647f1db3de3acf8fafe8284f1a4788e340d7e16
saji2000 <sajaddaneshmand79@gmail.com>
2023-05-06 23:53:40 -0600

    leetcode arrays, remove element not finished, thank God

--- practice.py
+++ practice.py
@@ -1,14 +1,11 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-
-        k = 0
         i = 0
-        while i < len(nums):
-            print(nums[i])
-            if(nums[i] == val):
-                del nums[i]
-                nums.append(0)
-                k += 1
-                i -= 1
-            i += 1
-        return len(nums) - k
+        j = 0
+        while j < len(nums):
+            print("j: " + str(j) + " nums[j]:"+ str(nums[j]) + " i: " + str(i) + " nums[i]: " + str(nums[i]))
+            if nums[j] != val:
+                nums[i] = nums[j]
+                i += 1
+            j += 1
+        return i
