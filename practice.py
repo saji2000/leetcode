83736b0e2772cf35844346d5780bba35458802cb
saji2000 <sajaddaneshmand79@gmail.com>
2023-05-08 19:01:19 -0600

    leetcode arrays, remove duplicate elements finished, thank God

--- practice.py
+++ practice.py
@@ -1,12 +1,10 @@
 class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
-        j = 0
-        i = 0
+    def removeDuplicates(self, nums: List[int]) -> int:
+        j = 1
 
-        while i < len(nums):
-            if(nums[i] != val):
+        for i in range(1, len(nums)):
+            if(nums[i] != nums[i - 1]):
                 nums[j] = nums[i]
                 j += 1
-            i += 1
 
         return j
