168acaf03e579d3de4d14e059ca540db6ce6a64e
saji2000 <sajaddaneshmand79@gmail.com>
2023-05-09 21:43:41 -0600

    leetcode arrays, Check If N and Its Double Exist works but unoptimized, thank God

--- practice.py
+++ practice.py
@@ -1,3 +1,15 @@
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
+
+        arr.sort()
+
+        for i in range(len(arr)):
+            for j in range(len(arr)):
+                if (i != j and arr[j] == arr[i] * 2): 
+                    return True
         
+        return False
+
+
+
+
