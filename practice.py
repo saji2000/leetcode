8fb392608bebe57d3477d53a40d72a643d93b222
saji2000 <sajaddaneshmand79@gmail.com>
2023-05-10 18:46:45 -0600

    leetcode arrays, mountain unoptimized, thank God

--- practice.py
+++ practice.py
@@ -1,17 +1,26 @@
+
 class Solution:
     def validMountainArray(self, arr: List[int]) -> bool:
 
+        if(len(arr) < 2):
+            return False
+
         ascend = True
-        for i in range(len(arr)):
+        descend = False
+
+        for i in range(len(arr)-1):
             if(ascend and arr[i] < arr[i+1]):
                 continue
             elif(not ascend and arr[i] > arr[i+1]):
                 continue
             elif(ascend and arr[i] > arr[i+1] and i != 0):
                 ascend = False
+                descend = True
                 continue
             else:
                 return False
         
-        return True
+        return descend
+
+
             
