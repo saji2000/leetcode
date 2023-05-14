1b1278b1cc85c216be3bd761e568b4fcc00d2998
saji2000 <sajaddaneshmand79@gmail.com>
2023-05-14 14:35:12 -0600

    leetcode arrays, remove zeroes finished, thank God

--- practice.py
+++ practice.py
@@ -3,11 +3,15 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        if(len(nums) < 2):
+            return
         i = 0
         j = 0
         while i < len(nums):
             if(nums[i] != 0):
+                temp = nums[j]
                 nums[j] = nums[i]
+                nums[i] = temp
                 j += 1
             i += 1
                 
