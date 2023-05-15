6b38ecde4ba8e5883d81ba8653eafbae2971d142
saji2000 <sajaddaneshmand79@gmail.com>
2023-05-15 14:03:56 -0600

    leetcode arrays, sorted by parit finished, thank God

--- practice.py
+++ practice.py
@@ -1,17 +1,17 @@
 class Solution:
-    def moveZeroes(self, nums: List[int]) -> None:
+    def sortArrayByParity(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
         if(len(nums) < 2):
-            return
+            return nums
         i = 0
         j = 0
         while i < len(nums):
-            if(nums[i] != 0):
+            if(nums[i]%2 == 0):
                 temp = nums[j]
                 nums[j] = nums[i]
                 nums[i] = temp
                 j += 1
             i += 1
-                
+        return nums
