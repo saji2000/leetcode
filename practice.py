233097d76fb816f7cb4b66e7e43b717c75adb158
saji2000 <sajaddaneshmand79@gmail.com>
2023-05-17 17:54:03 -0600

    leetcode arrays, third max height, thank God

--- practice.py
+++ practice.py
@@ -1,11 +1,3 @@
 class Solution:
-    def heightChecker(self, heights: List[int]) -> int:
-
-        expected = sorted(heights)
-        num = 0
-
-        for i in range(len(heights)):
-            if(heights[i] != expected[i]):
-                num += 1
+    def thirdMax(self, nums: List[int]) -> int:
         
-        return num
