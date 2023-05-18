33371cbf7307f74158548d21e89093d81e9f113b
saji2000 <sajaddaneshmand79@gmail.com>
2023-05-18 17:39:44 -0600

    leetcode arrays, third max working, thank God

--- practice.py
+++ practice.py
@@ -3,13 +3,18 @@ class Solution:
         
         max_list = [] 
 
-        max = nums[0]
-
         nums.sort()
+        
+        max = nums[0]
+        
+        ans = 0
 
         for i in range(len(nums)):
-            if(nums[i] > max):
+            if(nums[i] >= max):
                 max = nums[i]
                 max_list.append(max)
+                ans += 1
         
         print(max_list)
+    
+        return max_list[-3]
