d3248fadf44178ed62d3caa23b03caefe2fd6b3b
saji2000 <sajaddaneshmand79@gmail.com>
2023-05-18 17:37:25 -0600

    leetcode arrays, third max working, thank God

--- practice.py
+++ practice.py
@@ -1,3 +1,15 @@
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
         
+        max_list = [] 
+
+        max = nums[0]
+
+        nums.sort()
+
+        for i in range(len(nums)):
+            if(nums[i] > max):
+                max = nums[i]
+                max_list.append(max)
+        
+        print(max_list)
