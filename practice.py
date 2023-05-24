6cd550e3dd533e8e30bac72c178d94ea66b62183
saji2000 <sajaddaneshmand79@gmail.com>
2023-05-23 18:56:50 -0600

    leetcode arrays, find disappeard numbers optimized solution with sets, thank God

--- practice.py
+++ practice.py
@@ -1,3 +1,12 @@
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+
+        numbers = set(nums)
+
+        ans = []
+
+        for i in range(1, len(nums) + 1):
+            if(i  not in numbers):
+                ans.append(i)
         
+        return ans
