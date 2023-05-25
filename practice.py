d4fa8fcad37f0f3153e8d0ba686e144f485d7890
saji2000 <sajaddaneshmand79@gmail.com>
2023-05-24 21:44:33 -0600

    leetcode LinkedLists, creating a linked list, thank God

--- practice.py
+++ practice.py
@@ -1,8 +1,33 @@
-class Solution:
-    def sortedSquares(self, nums: List[int]) -> List[int]:
-        for i in range(len(nums)):
-            nums[i] = nums[i] * nums[i]
-        nums.sort()
+class Node:
+    def __init__(self):
+        self.val = None
+        self.next = None
 
-        return nums
+class MyLinkedList:
 
+    def __init__(self):
+        self.node = Node()
+
+    def get(self, index: int) -> int:
+        
+
+    def addAtHead(self, val: int) -> None:
+        
+
+    def addAtTail(self, val: int) -> None:
+        
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        
+
+    def deleteAtIndex(self, index: int) -> None:
+        
+
+
+# Your MyLinkedList object will be instantiated and called as such:
+# obj = MyLinkedList()
+# param_1 = obj.get(index)
+# obj.addAtHead(val)
+# obj.addAtTail(val)
+# obj.addAtIndex(index,val)
+# obj.deleteAtIndex(index)
