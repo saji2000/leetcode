11948efa15daa95353e7145660c42369f6db9d97
saji2000 <sajaddaneshmand79@gmail.com>
2023-06-05 23:01:29 -0600

    get intersection node done easily with sets, thank God, praise God

--- practice.py
+++ practice.py
@@ -7,3 +7,17 @@
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
         
+        nodes = set()
+
+        node_a = headA
+        node_b = headB
+
+        while(node_a):
+            nodes.add(node_a)
+            node_a = node_a.next
+
+        while(node_b):
+            if(node_b in nodes):
+                return node_b
+            node_b = node_b.next
+        return None
