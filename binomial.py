36666b2797795b0c1a7b12d8384cd58529da5f04
saji2000 <sajaddaneshmand79@gmail.com>
2023-06-12 18:36:05 -0600

    adding coins and dices, thank God, praise God

--- binomial.py
+++ binomial.py
@@ -15,4 +15,12 @@ formatted_probability = "{:.11f}".format(probability * 100)
 # Print the result
 print("Probability: " + formatted_probability + "%")
 
-print("1 in :" + str(1/probability))
+print("1 in :" + str(round(1/probability)))
+
+log = math.log(1/probability, 2)
+
+print("Similar to tossing a coin", round(log), "times and getting only heads")
+
+log = math.log(1/probability, 6)
+
+print("Similar to tossing a dice", round(log), "times and getting only 6")
