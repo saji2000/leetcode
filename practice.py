97abbe176a300f4dcaa670c67943051e1dbfbc31
saji2000 <sajaddaneshmand79@gmail.com>
2023-06-13 13:52:44 -0600

    merge two sorted lists done, thank God, praise God

--- practice.py
+++ practice.py
@@ -1,20 +1,27 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution:
-    def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        
-        node = head
-        nodes = []
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
-        while(node):
-            nodes.append(node.val)
-            node = node.next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+
+        ans = ListNode(0)
+        ans_node = ans
+
+        while(list1 and list2):
+            if(list1.val <= list2.val):
+                ans_node.next = list1
+                list1 = list1.next
+            else:
+                ans_node.next = list2
+                list2 = list2.next
+            ans_node = ans_node.next
         
-        for j in range(len(nodes)):
-            if(nodes[j] != nodes[-j-1]):
-                return False
-            
-        return True
+        if(list1):
+            ans_node.next = list1
+        else:
+            ans_node.next = list2
+
+        return ans.next
