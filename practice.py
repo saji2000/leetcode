88834be6fc9b13d786fb6aabddb057ede5a041e1
saji2000 <sajaddaneshmand79@gmail.com>
2023-06-12 18:47:58 -0600

    isPalindrome with linked list is doen, thank God, praise God

--- practice.py
+++ practice.py
@@ -1,22 +1,20 @@
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if head is None:
-            return None
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        
+        node = head
+        nodes = []
 
-        odd = head
-        even = head.next
-        even_head = even
-
-        while even and even.next:
-            odd.next = even.next
-            odd = odd.next
-            even.next = odd.next
-            even = even.next
-
-        odd.next = even_head
-        return head
+        while(node):
+            nodes.append(node.val)
+            node = node.next
+        
+        for j in range(len(nodes)):
+            if(nodes[j] != nodes[-j-1]):
+                return False
+            
+        return True
