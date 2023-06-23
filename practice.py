71162b06f5a27637988fcb9a38d94c030e421caa
saji2000 <sajaddaneshmand79@gmail.com>
2023-06-23 14:21:36 -0600

    acceptable solution for pascal triangle recursion method

--- practice.py
+++ practice.py
@@ -2,12 +2,18 @@ class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
         
         ans = []
+
+        memo = {}
         
         def rowColumn(row, column):
             if(column == 0 or row == column):
                 return 1
+            elif (row, column) in memo:
+                return memo[(row, column)]
             else:
-                return rowColumn(row - 1, column - 1) + rowColumn(row - 1, column)
+                result = rowColumn(row - 1, column - 1) + rowColumn(row - 1, column)
+                memo[(row, column)] = result
+                return result
             
         
         for i in range(0, rowIndex + 1):
