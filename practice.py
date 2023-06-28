fe1ab4d78d7c2c10aece5158fd8b5b4c11274c13
saji2000 <sajaddaneshmand79@gmail.com>
2023-06-28 14:14:39 -0600

    merge two lists recursively

--- practice.py
+++ practice.py
@@ -1,22 +1,36 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        val = val
+        next = next
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         val = val
+#         next = next
 class Solution:
-    def myPow(self, x: float, n: int) -> float:
 
-        if n == 0:
-            return 1
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+
+        ans = None
+
+        if(not list2):
+            return list1
+        elif(not list1):
+            return list2
         
-        if n == 1:
-            return x
-
-        if n < 0:
-            x = 1 / x
-            n = -n
-
-        if n == 2:
-            return x * x
         
-        half = self.myPow(x, n // 2)
+        if(list1.val <= list2.val):
+            ans = list1
+            ans.next = self.mergeTwoLists(list1.next, list2)
 
-        if n % 2 == 0:
-            return half * half
         else:
-            return half * half * x
+            ans = list2
+            ans.next = self.mergeTwoLists(list1, list2.next)
+
+        return ans
+
+
+
+
