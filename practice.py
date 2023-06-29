5b2199c8bc2190c56c2b2aeda1f36d4c4b90c9d4
saji2000 <sajaddaneshmand79@gmail.com>
2023-06-29 13:48:52 -0600

    two sums optimized with dictionary

--- practice.py
+++ practice.py
@@ -1,10 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
 
+        seen = {}
+
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in seen:
+                return (seen[complement], i)
+            seen[num] = i
         
-        for i in range(0, len(nums)):
-            for j in range(i + 1, len(nums)):
-                if(nums[i] + nums[j] == target):
-                    return (i, j)
-                
         return None
