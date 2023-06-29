7dcb9f579b609bf70bac0f7caec9cc34ad440b6b
saji2000 <sajaddaneshmand79@gmail.com>
2023-06-29 13:36:09 -0600

    two sums with two for loops

--- practice.py
+++ practice.py
@@ -1,36 +1,10 @@
-# Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        val = val
-        next = next
-
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         val = val
-#         next = next
 class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
 
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-
-        ans = None
-
-        if(not list2):
-            return list1
-        elif(not list1):
-            return list2
         
-        
-        if(list1.val <= list2.val):
-            ans = list1
-            ans.next = self.mergeTwoLists(list1.next, list2)
-
-        else:
-            ans = list2
-            ans.next = self.mergeTwoLists(list1, list2.next)
-
-        return ans
-
-
-
-
+        for i in range(0, len(nums)):
+            for j in range(i + 1, len(nums)):
+                if(nums[i] + nums[j] == target):
+                    return (i, j)
+                
+        return None
