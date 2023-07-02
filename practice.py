ffadb190144538d68788089e05710ed2020982d2
saji2000 <sajaddaneshmand79@gmail.com>
2023-07-02 12:27:53 -0600

    majority elemnt optimized

--- practice.py
+++ practice.py
@@ -1,25 +1,5 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        
-        if len(nums) == 0:
-            return None
-        elif len(nums) == 1:
-            return nums[0]
-        
-        memo = {}
-        
-        for num in nums:
-            if num in memo:
-                memo[num] += 1
-            else:
-                memo[num] = 1
-            
-        max = 1
-        ans = 0
-        for num in memo:
-            if memo[num] > max:
-                max = memo[num]
-                ans = num
-        
-        return ans
-
+        nums.sort()
+        n = len(nums)
+        return nums[n//2]
