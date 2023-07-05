77eb56e7121466012f5c6afe160eeddc3da719f7
saji2000 <sajaddaneshmand79@gmail.com>
2023-07-05 17:12:47 -0600

    roman to int fast solution, praise God

--- practice.py
+++ practice.py
@@ -2,29 +2,30 @@ class Solution:
     def romanToInt(self, s: str) -> int:
         sum = 0
 
+        memo = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
+
         for i in reversed(range(0, len(s))):
-            if s[i] == "M":
-                sum += 1000
-            elif s[i] == "D":
-                sum += 500
-            elif s[i] == "C":
-                if (i < len(s) - 1) and (s[i + 1] == "D" or s[i + 1] == "M"):
-                    sum -= 100
-                else:
-                    sum += 100
-            elif s[i] == "L":
-                sum += 50
-            elif s[i] == "X":
-                if i < (len(s) - 1) and (s[i + 1] == "L" or s[i + 1] == "C"):
-                    sum -= 10
-                else:
-                    sum += 10
-            elif s[i] == "V":
-                sum += 5
-            elif s[i] == "I":
-                if i < (len(s) - 1) and (s[i + 1] == "V" or s[i + 1] == "X"):
-                    sum -= 1
-                else:
-                    sum += 1
+            if (i < len(s) - 1) and (
+                s[i] == "C" and (s[i + 1] == "D" or s[i + 1] == "M")
+            ):
+                sum -= 100
+
+            elif (i < len(s) - 1) and (
+                s[i] == "X" and (s[i + 1] == "L" or s[i + 1] == "C")
+            ):
+                sum -= 10
+
+            elif (i < len(s) - 1) and (
+                s[i] == "I" and (s[i + 1] == "V" or s[i + 1] == "X")
+            ):
+                sum -= 1
+
+            else:
+                sum += memo[s[i]]
 
         return sum
+
+
+solution = Solution()
+
+print(solution.romanToInt("MCMXCIV"))
