1d7736f063d50f3fd3ba5ce44ae30a46a053a997
saji2000 <sajaddaneshmand79@gmail.com>
2023-07-05 17:01:27 -0600

    roman to int slow solution

--- practice.py
+++ practice.py
@@ -1,17 +1,30 @@
 class Solution:
-    def maxProfit(self,prices):
-     
-        l = 0
-        r = 1
-        profit = 0
-        max_profit = 0 
+    def romanToInt(self, s: str) -> int:
+        sum = 0
 
-        while r < len(prices):
-            profit = prices[r] - prices[l]
+        for i in reversed(range(0, len(s))):
+            if s[i] == "M":
+                sum += 1000
+            elif s[i] == "D":
+                sum += 500
+            elif s[i] == "C":
+                if (i < len(s) - 1) and (s[i + 1] == "D" or s[i + 1] == "M"):
+                    sum -= 100
+                else:
+                    sum += 100
+            elif s[i] == "L":
+                sum += 50
+            elif s[i] == "X":
+                if i < (len(s) - 1) and (s[i + 1] == "L" or s[i + 1] == "C"):
+                    sum -= 10
+                else:
+                    sum += 10
+            elif s[i] == "V":
+                sum += 5
+            elif s[i] == "I":
+                if i < (len(s) - 1) and (s[i + 1] == "V" or s[i + 1] == "X"):
+                    sum -= 1
+                else:
+                    sum += 1
 
-            if profit > 0:
-                max_profit = max(profit, max_profit)
-            else:
-                l = r
-            r += 1
-        return max_profit
+        return sum
