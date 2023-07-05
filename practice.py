9328d0198a50c67280586f90f47a1d94f3b8ffe3
saji2000 <sajaddaneshmand79@gmail.com>
2023-07-05 17:17:28 -0600

    roman to int very fast solution, praise God

--- practice.py
+++ practice.py
@@ -4,22 +4,9 @@ class Solution:
 
         memo = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
 
-        for i in reversed(range(0, len(s))):
-            if (i < len(s) - 1) and (
-                s[i] == "C" and (s[i + 1] == "D" or s[i + 1] == "M")
-            ):
-                sum -= 100
-
-            elif (i < len(s) - 1) and (
-                s[i] == "X" and (s[i + 1] == "L" or s[i + 1] == "C")
-            ):
-                sum -= 10
-
-            elif (i < len(s) - 1) and (
-                s[i] == "I" and (s[i + 1] == "V" or s[i + 1] == "X")
-            ):
-                sum -= 1
-
+        for i in range(0, len(s)):
+            if i < (len(s) - 1) and memo[s[i]] < memo[s[i + 1]]:
+                sum -= memo[s[i]]
             else:
                 sum += memo[s[i]]
 
