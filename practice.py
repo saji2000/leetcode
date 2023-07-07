9e43fcf0a525f120d99246ff168e1fbb312bb67f
saji2000 <sajaddaneshmand79@gmail.com>
2023-07-06 18:06:01 -0600

    length of last word slow solution, praise God

--- practice.py
+++ practice.py
@@ -1,18 +1,10 @@
 class Solution:
-    def romanToInt(self, s: str) -> int:
-        sum = 0
+    def lengthOfLastWord(self, s: str) -> int:
+        arr = s.split()
 
-        memo = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
-
-        for i in range(0, len(s)):
-            if i < (len(s) - 1) and memo[s[i]] < memo[s[i + 1]]:
-                sum -= memo[s[i]]
-            else:
-                sum += memo[s[i]]
-
-        return sum
+        return len(arr[-1])
 
 
 solution = Solution()
 
-print(solution.romanToInt("MCMXCIV"))
+print(solution.lengthOfLastWord("MCMXCIV"))
