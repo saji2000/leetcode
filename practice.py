d93567932842f239e72e053d3a98508923305060
saji2000 <sajaddaneshmand79@gmail.com>
2023-07-10 14:10:23 -0600

    anagram using dictionary, praise God

--- practice.py
+++ practice.py
@@ -1,10 +1,24 @@
 class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        seen = set()
+    def isAnagram(self, s: str, t: str) -> bool:
+        len_t = len(t)
+        len_s = len(s)
 
-        for i in nums:
-            if i not in seen:
-                seen.add(i)
+        if len_t != len_s:
+            return False
+
+        memo = {}
+        for i in range(0, len_s):
+            if s[i] not in memo:
+                memo[s[i]] = 1
             else:
-                return True
-        return False
+                memo[s[i]] += 1
+
+        for i in range(0, len_t):
+            if t[i] not in memo:
+                return False
+            else:
+                if memo[t[i]] < 1:
+                    return False
+                memo[t[i]] -= 1
+
+        return True
