9945d797bd1a0299321214370e1fb6061ae65fd6
saji2000 <sajaddaneshmand79@gmail.com>
2023-07-10 14:10:37 -0600

    anagram using sort, praise God

--- practice.py
+++ practice.py
@@ -1,24 +1,5 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        len_t = len(t)
-        len_s = len(s)
-
-        if len_t != len_s:
-            return False
-
-        memo = {}
-        for i in range(0, len_s):
-            if s[i] not in memo:
-                memo[s[i]] = 1
-            else:
-                memo[s[i]] += 1
-
-        for i in range(0, len_t):
-            if t[i] not in memo:
-                return False
-            else:
-                if memo[t[i]] < 1:
-                    return False
-                memo[t[i]] -= 1
-
-        return True
+        sorted_s = sorted(s)
+        sorted_t = sorted(t)
+        return sorted_s == sorted_t
