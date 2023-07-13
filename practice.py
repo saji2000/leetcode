aae9136db0c9140047bdf4841d59783fb50cd7ac
saji2000 <sajaddaneshmand79@gmail.com>
2023-07-12 22:31:32 -0600

    validpalindrome using isalnum method, praise God

--- practice.py
+++ practice.py
@@ -3,7 +3,8 @@ import re
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = "".join(val for val in s if val.isalnum()).lower()
+        s = "".join(val for val in s if val.isalnum().lower())
+        return s == s[::-1]
 
 
 solution = Solution()
