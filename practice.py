b6d6971105f41d0576401ff6a5d799fd3ffbf65a
saji2000 <sajaddaneshmand79@gmail.com>
2023-07-14 21:12:50 -0600

    group anagrams using default dict and list, praise God

--- practice.py
+++ practice.py
@@ -1,10 +1,8 @@
 class Solution:
-    def maxProfit(self, prices):
-        l, r = 0
-        profit = 0
-        while r < len(prices):
-            if prices[l] > prices[r]:
-                l = r
-            profit = max(profit, prices[r] - prices[l])
-            r += 1
-        return profit
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = defaultdict(list)
+
+        for word in strs:
+            groups["".join(sorted(word))] = word
+
+        return [groups.values()]
