446beca37b9c750b8587ecb361d92306e4bf08a7
saji2000 <sajaddaneshmand79@gmail.com>
2023-07-15 23:59:28 -0600

    top frequent k, praise God

--- practice.py
+++ practice.py
@@ -1,8 +1,18 @@
 class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        groups = defaultdict(list)
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq = {}
 
-        for word in strs:
-            groups["".join(sorted(word))] = word
+        for i in nums:
+            if i in freq:
+                freq[i] += 1
+            else:
+                freq[i] = 1
 
-        return [groups.values()]
+        sorted_nums = sorted(freq.keys(), key=lambda x: freq[x], reverse=True)
+
+        return sorted_nums[:k]
+
+
+solution = Solution()
+
+solution.topKFrequent([[1, 1, 1, 2, 2, 3]])
