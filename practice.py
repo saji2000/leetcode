3ea3a24b49211054d4a17c996987be88072f6e6a
saji2000 <sajaddaneshmand79@gmail.com>
2023-07-16 22:42:42 -0600

    brackets validity, praise God

--- practice.py
+++ practice.py
@@ -1,12 +1,16 @@
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        freq = Counter(nums)
-
-        sorted_nums = sorted(freq.keys(), key=lambda x: freq[x], reverse=True)
-
-        return sorted_nums[:k]
-
-
-solution = Solution()
-
-solution.topKFrequent([[1, 1, 1, 2, 2, 3]])
+class Solution(object):
+    def isValid(self, s):
+        stack = []
+        for c in s:
+            if c in "([{":
+                stack.append(c)
+            else:
+                if (
+                    not stack
+                    or (c == ")" and stack[-1] != "(")
+                    or (c == "}" and stack[-1] != "{")
+                    or (c == "]" and stack[-1] != "[")
+                ):
+                    return False
+                stack.pop()
+        return not stack
