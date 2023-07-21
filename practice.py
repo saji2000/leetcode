5efb785b3b4cf3156453d54d09385f42c7ba24fd
saji2000 <sajaddaneshmand79@gmail.com>
2023-07-21 12:36:40 -0600

    group anagrams review, praise God

--- practice.py
+++ practice.py
@@ -1,16 +1,9 @@
-class Solution(object):
-    def isValid(self, s):
-        stack = []
-        for c in s:
-            if c in "([{":
-                stack.append(c)
-            else:
-                if (
-                    not stack
-                    or (c == ")" and stack[-1] != "(")
-                    or (c == "}" and stack[-1] != "{")
-                    or (c == "]" and stack[-1] != "[")
-                ):
-                    return False
-                stack.pop()
-        return not stack
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = defaultdict(list)
+
+        for i in strs:
+            sorted_word = "".join(sorted(i))
+            groups[sorted_word].append(i)
+
+        return list(groups.values())
