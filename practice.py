bcb4c18fdd4e5ce062c0fb8c612a2ab10f5d2856
saji2000 <sajaddaneshmand79@gmail.com>
2023-08-03 19:30:31 -0600

    invert binary tree done, thank God

--- practice.py
+++ practice.py
@@ -16,9 +16,7 @@ class Solution:
         temp = root.left
         root.left = root.right
         root.right = temp
-        # if root.left:
         self.invertTree(root.left)
 
-        # if root.right:
-        self.invertTree(root.left)
+        self.invertTree(root.right)
         return root
