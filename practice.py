132b4c0c9bf7d8b071fdb506710c912d86a34672
saji2000 <sajaddaneshmand79@gmail.com>
2023-08-03 19:31:20 -0600

    invert binary tree cleaned, thank God

--- practice.py
+++ practice.py
@@ -10,13 +10,12 @@ class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root:
             return root
-        if not root.left and not root.right:
-            return root
 
         temp = root.left
         root.left = root.right
         root.right = temp
-        self.invertTree(root.left)
 
+        self.invertTree(root.left)
         self.invertTree(root.right)
+
         return root
