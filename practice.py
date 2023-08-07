99375fda82b45039ee507ae16abc8bb73d891171
saji2000 <sajaddaneshmand79@gmail.com>
2023-08-06 23:16:57 -0600

    is sub tree harder but efficient solution, thank God

--- practice.py
+++ practice.py
@@ -7,7 +7,20 @@ class TreeNode:
 
 
 class Solution:
-    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        if str(subRoot) in str(root):
+    def isMatch(self, rootA: Optional[TreeNode], rootB: Optional[TreeNode]):
+        if not (rootA and rootB):
+            return rootA is rootB
+        elif (
+            (rootA.val == rootB.val)
+            and (self.isMatch(rootA.left, rootB.left))
+            and (self.isMatch(rootA.right, rootB.right))
+        ):
             return True
         return False
+
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if self.isMatch(root, subRoot):
+            return True
+        elif not root:
+            return False
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
