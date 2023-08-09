f88e117b7faf9fea5d3e5115e8d58c5e250688cb
saji2000 <sajaddaneshmand79@gmail.com>
2023-08-08 23:20:28 -0600

    sub tree review, thank God

--- practice.py
+++ practice.py
@@ -1,20 +1,26 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 class Solution:
-    memo = {}
+    def isMatch(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]):
+        if not root and not subRoot:
+            return True
+        if (not root) ^ (not subRoot):
+            return False
+        if root.val == subRoot.val:
+            return self.isMatch(root.left, subRoot.left) and self.isMatch(
+                root.right, subRoot.right
+            )
+        return False
 
-    def climbStairs(self, n: int) -> int:
-        if n == 1:
-            return 1
-        if n == 2:
-            return 2
-        if (n - 1) in self.memo:
-            num_1 = self.memo[n - 1]
-        else:
-            self.memo[n - 1] = self.climbStairs(n - 1)
-            num_1 = self.memo[n - 1]
-        if (n - 2) in self.memo:
-            num_2 = self.memo[n - 2]
-        else:
-            self.memo[n - 2] = self.climbStairs(n - 2)
-            num_2 = self.memo[n - 2]
-
-        return num_1 + num_2
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if root is None:
+            return False
+        if self.isMatch(root, subRoot):
+            return True
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
