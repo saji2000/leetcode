544d9298b436204719b1aca8ca6313d0ea62a1df
saji2000 <sajaddaneshmand79@gmail.com>
2023-08-10 23:26:22 -0600

    count bits started, thank God

--- practice.py
+++ practice.py
@@ -1,7 +1,2 @@
 class Solution:
-    def hammingWeight(self, n: int) -> int:
-        count = 0
-        while n:
-            count += n % 2
-            n = n >> 1
-        return count
+    def countBits(self, n: int) -> List[int]:
