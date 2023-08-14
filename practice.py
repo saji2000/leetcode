d3e0e65dd277fae4c15ef3460ba9d47b2efbe09a
saji2000 <sajaddaneshmand79@gmail.com>
2023-08-13 23:30:34 -0600

    hamming weight using bit shifting review, thank God

--- practice.py
+++ practice.py
@@ -1,2 +1,8 @@
 class Solution:
-    def countBits(self, n: int) -> List[int]:
+    def hammingWeight(self, n: int) -> int:
+        count = 0
+        while n:
+            if n % 2 != 0:
+                count += 1
+            n = n >> 1
+        return n
