f403e735a6644898c753c22b0d33dc0a04828628
saji2000 <sajaddaneshmand79@gmail.com>
2023-08-16 23:36:31 -0600

    reverse bits, thank God

--- practice.py
+++ practice.py
@@ -1,10 +1,3 @@
 class Solution:
-    def countBits(self, n: int) -> List[int]:
-        ans = []
-        for i in range(0, n + 1):
-            count = 0
-            while i:
-                count += i % 2
-                i = i >> 1
-            ans.append(count)
-        return ans
+    def reverseBits(self, n: int) -> int:
+        
