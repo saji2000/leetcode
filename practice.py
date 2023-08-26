5e56310193877045a09bf85d912b38370c034fd8
saji2000 <sajaddaneshmand79@gmail.com>
2023-08-26 13:09:17 -0600

    product except self analyzed, thank God

--- practice.py
+++ practice.py
@@ -1,16 +1,30 @@
 class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
+    def productExceptSelf(self, nums: list[int]) -> list[int]:
         n = len(nums)
 
+        # Initialize output array
         result = [1] * n
-        left_product = 1
-        for i in range(0, n):
-            result[i] = left_product
-            left_product *= nums[i]
 
+        # Compute left products
+        left_product = 1
+        for i in range(n):
+            result[i] *= left_product
+            left_product *= nums[i]
+            print("Left Product: ", left_product)
+            print("Left: ", result)
+
+        # Compute right products and combine with left products
         right_product = 1
         for i in range(n - 1, -1, -1):
             result[i] *= right_product
             right_product *= nums[i]
+            print("Right Product: ", right_product)
+            print("Right: ", result)
 
         return result
+
+
+nums = [1, 2, 3, 4]
+solution = Solution()
+output = solution.productExceptSelf(nums)
+print(output)  # Output: [24, 12, 8, 6]
