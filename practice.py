42348a0ac6e5598256c7669b119c550a45467b5e
saji2000 <sajaddaneshmand79@gmail.com>
2023-08-29 13:40:37 -0600

    longest consecutive, thank God

--- practice.py
+++ practice.py
@@ -1,30 +1,17 @@
 class Solution:
-    def productExceptSelf(self, nums: list[int]) -> list[int]:
-        n = len(nums)
-
-        # Initialize output array
-        result = [1] * n
-
-        # Compute left products
-        left_product = 1
-        for i in range(n):
-            result[i] *= left_product
-            left_product *= nums[i]
-            print("Left Product: ", left_product)
-            print("Left: ", result)
-
-        # Compute right products and combine with left products
-        right_product = 1
-        for i in range(n - 1, -1, -1):
-            result[i] *= right_product
-            right_product *= nums[i]
-            print("Right Product: ", right_product)
-            print("Right: ", result)
-
-        return result
-
-
-nums = [1, 2, 3, 4]
-solution = Solution()
-output = solution.productExceptSelf(nums)
-print(output)  # Output: [24, 12, 8, 6]
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        nums.sort()
+        longest = 1
+        length = 1
+        for i in range(len(nums) - 1):
+            if nums[i] + 1 == nums[i + 1]:
+                length += 1
+            elif nums[i] == nums[i + 1]:
+                continue
+            else:
+                length = 1
+            if length > longest:
+                longest = length
+        return longest
