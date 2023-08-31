18f404ebd969d3b58c4b04cb9c19d64797793e57
saji2000 <sajaddaneshmand79@gmail.com>
2023-08-30 22:39:31 -0600

    3 sum started, thank God

--- practice.py
+++ practice.py
@@ -1,17 +1,2 @@
 class Solution:
-    def longestConsecutive(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
-        nums.sort()
-        longest = 1
-        length = 1
-        for i in range(len(nums) - 1):
-            if nums[i] + 1 == nums[i + 1]:
-                length += 1
-            elif nums[i] == nums[i + 1]:
-                continue
-            else:
-                length = 1
-            if length > longest:
-                longest = length
-        return longest
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
