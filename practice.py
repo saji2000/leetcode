ce5615d312d022756e88713ddeaaa8e3b051430b
saji2000 <sajaddaneshmand79@gmail.com>
2023-09-03 23:28:55 -0600

    3 sum review, thank God

--- practice.py
+++ practice.py
@@ -1,30 +1,3 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        if not nums:
-            return 0
-
-        ans = []
-        nums.sort()
-
-        for i, a in enumerate(nums):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
-            l = i + 1
-            r = len(nums) - 1
-            while l < r:
-                threeSum = a + nums[l] + nums[r]
-
-                if threeSum > 0:
-                    r -= 1
-                elif threeSum < 0:
-                    l += 1
-                else:
-                    ans.append([a, nums[l], nums[r]])
-                    r -= 1
-                    l += 1
-                    while r > l and nums[r] == nums[r + 1]:
-                        r -= 1
-                    while r > l and nums[l] == nums[l - 1]:
-                        l += 1
-
-        return ans
+        
