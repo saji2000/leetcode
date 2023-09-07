fcbf4692bb26a45273fcd1191c62d274b4125f85
saji2000 <sajaddaneshmand79@gmail.com>
2023-09-06 21:18:24 -0600

    max area done, thank God

--- practice.py
+++ practice.py
@@ -1,3 +1,14 @@
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        
+    def maxArea(self, height: List[int]) -> int:
+        l, r = 0, len(height) - 1
+        max_area = 0
+
+        while l < r:
+            area = min(height[l], height[r]) * (r - l)
+            max_area = max(max_area, area)
+
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+        return max_area
