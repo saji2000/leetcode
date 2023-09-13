5bae250f7efd67afb0a97546d6e48822d76b828f
saji2000 <sajaddaneshmand79@gmail.com>
2023-09-12 23:54:23 -0600

    Longest substring started, thank God

--- practice.py
+++ practice.py
@@ -1,15 +1,5 @@
 class Solution:
-    def maxArea(self, height: List[int]) -> int:
-        l, r = 0, len(height) - 1
-        maxArea = 0
-
-        while l < r:
-            area = min(height[l], height[r]) * (r - l)
-            maxArea = max(maxArea, area)
-
-            if height[l] < height[r]:
-                l += 1
-            else:
-                r -= 1
-
-        return maxArea
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        n = len(s)
+        maxLength = 0
+        my_set = set()
