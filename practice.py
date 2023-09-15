f35518542fed733f2fed4c13519795c5c6b069d8
saji2000 <sajaddaneshmand79@gmail.com>
2023-09-14 22:01:57 -0600

    Longest substring finished, thank God

--- practice.py
+++ practice.py
@@ -1,3 +1,15 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        n = len(s)
+        maxLength = 0
+        charSet = set()
+        l = 0
 
+        for r in range(n):
+            while s[r] in charSet:
+                charSet.remove(s[l])
+                l += 1
+            charSet.add(s[r])
+            maxLength = max(maxLength, r - l + 1)
+
+        return maxLength
