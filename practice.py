c5735c8512066689db54ebdeb072c9cf8d622094
saji2000 <sajaddaneshmand79@gmail.com>
2023-09-18 23:09:41 -0600

    char replacement started, thank God

--- practice.py
+++ practice.py
@@ -1,14 +1,3 @@
 class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        n = len(s)
-        charSet = set()
-        maxLength = 0
-        l = 0
-
-        for r in range(n):
-            while s[r] in charSet:
-                charSet.remove(s[l])
-                l += 1
-            charSet.add(s[r])
-            maxLength = max(maxLength, r - l + 1)
-        return maxLength
+    def characterReplacement(self, s: str, k: int) -> int:
+        
