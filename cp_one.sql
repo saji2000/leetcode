01463049f96262d16ee740afea79761900c57665
saji2000 <sajaddaneshmand79@gmail.com>
2023-10-13 19:29:29 -0600

    Preparing for interview by practicing SQL queries

--- cp_one.sql
+++ cp_one.sql
@@ -14,3 +14,18 @@ SELECT MAX(Salary), MIN(Salary), AVG(Salary) FROM Salary
 SELECT EmpId
 FROM Salary
 WHERE Salary >= 3000 AND Salary <= 4000
+
+SELECT *
+FROM Employee
+WHERE ManagerId = 986 AND City = 'Chennai'
+
+SELECT *
+FROM Employee
+WHERE ManagerId = 321 OR City = 'Chennai'
+
+SELECT *
+FROM Salary
+WHERE Project != 'P1'
+
+SELECT EmpId, Salary + Variable as TotalSalary
+FROM Salary
