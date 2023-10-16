3ac88d77638a4a02cfba58d39a550270236fb272
saji2000 <sajaddaneshmand79@gmail.com>
2023-10-16 12:41:25 -0600

    Summary ranges

--- practice.py
+++ practice.py
@@ -1,3 +1,25 @@
 class Solution:
-    def characterReplacement(self, s: str, k: int) -> int:
-        
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        ans = []
+
+        if not nums:
+            return ans
+
+        l = 0
+        r = 0
+
+        while r < len(nums) - 1:
+            if nums[r] != nums[r + 1] - 1:
+                if l == r:
+                    ans.append(str(nums[l]))
+                else:
+                    ans.append(str(nums[l]) + "->" + str(nums[r]))
+                l = r + 1
+            r += 1
+
+        if l == r:
+            ans.append(str(nums[l]))
+        else:
+            ans.append(str(nums[l]) + "->" + str(nums[r]))
+
+        return ans
