f86cd6a2f2ba59535036e13ef8d9b805d0f41cb8
saji2000 <sajaddaneshmand79@gmail.com>
2023-10-17 00:38:48 -0600

    odd even list

--- practice.py
+++ practice.py
@@ -1,20 +1,25 @@
-# Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
         self.val = val
-        self.left = left
-        self.right = right
+        self.next = next
 
 
 class Solution:
-    def pruneTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        if not root:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None:
             return
 
-        root.left = self.pruneTree(root.left)
-        root.right = self.pruneTree(root.right)
+        odd = head
+        even = odd.next
+        even_head = head.next
 
-        if root.val == 0 and root.right is None and root.left is None:
-            return None
+        while even and even.next:
+            odd.next = odd.next.next
+            odd = odd.next
+            even.next = even.next.next
+            even = even.next
 
-        return root
+        odd.next = even_head
+
+        return head
