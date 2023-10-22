5bff7f2509cc6cc7319613c64c394e786436b523
saji2000 <sajaddaneshmand79@gmail.com>
2023-10-22 14:23:56 -0600

    reviews

--- WordPattern.py
+++ WordPattern.py
@@ -2,18 +2,15 @@ class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
         s = s.split()
 
-        if len(s) != len(pattern):
-            return False
-
-        if len(set(s)) != len(set(pattern)):
+        if len(s) != len(pattern) or len(set(s)) != len(set(pattern)):
             return False
 
         my_dict = {}
 
-        for i in range(len(pattern)):
+        for i in range(len(s)):
             if pattern[i] in my_dict:
                 if my_dict[pattern[i]] != s[i]:
                     return False
-            my_dict[pattern[i]] = s[i]
-
+            else:
+                my_dict[pattern[i]] = s[i]
         return True
