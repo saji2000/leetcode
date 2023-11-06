2fe68209f44c62ce6c913eaa7fc6ab549bec0a5e
saji2000 <sajaddaneshmand79@gmail.com>
2023-11-05 20:56:08 -0700

    clean up

--- tutorial.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-years = [2015 + x for x in range(8)]
-weights = [78, 79, 80, 81, 80, 79, 80, 82]
-
-plt.plot(years, weights, lw=3, linestyle=":")
-
-plt.show()
