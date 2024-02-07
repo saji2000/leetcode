307abda7e9f37a5b1fc333321f30de412f8252a6
saji2000 <sajaddaneshmand79@gmail.com>
2024-02-07 11:52:43 -0500

    Contains duplicate finished, praise God

---
+++ leetcode.py
@@ -0,0 +1,8 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        mySet = set()
+        for i in nums:
+            if i in mySet:
+                return True
+            mySet.add(i)
+        
