579a0e9f454c5312cb1439c2c468f8ece9ef0511
saji2000 <sajaddaneshmand79@gmail.com>
2024-02-08 10:38:21 -0500

    isAnagram using dictionaries finished, praise God

--- leetcode.py
+++ leetcode.py
@@ -1,6 +1,22 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if Counter(s) == Counter(t):
+        myDict = dict()
+        myDict_t = dict()
+        for char in s:
+            if char in myDict:
+                myDict[char] += 1
+            else:
+                myDict[char] = 1
+        
+        for char in t:
+            if char in myDict_t:
+                myDict_t[char] += 1
+            else:
+                myDict_t[char] = 1
+
+        if myDict == myDict_t:
             return True
         return False
+        
+
         
