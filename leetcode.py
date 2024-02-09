780cec78c12ef987ede0f2241b4bd847d79730de
saji2000 <sajaddaneshmand79@gmail.com>
2024-02-09 12:14:01 -0500

    twoSum using dictionaries finished, praise God

--- leetcode.py
+++ leetcode.py
@@ -1,22 +1,13 @@
 class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        myDict = dict()
-        myDict_t = dict()
-        for char in s:
-            if char in myDict:
-                myDict[char] += 1
-            else:
-                myDict[char] = 1
-        
-        for char in t:
-            if char in myDict_t:
-                myDict_t[char] += 1
-            else:
-                myDict_t[char] = 1
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_map = {}
 
-        if myDict == myDict_t:
-            return True
-        return False
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_map:
+                return [i, num_map[complement]]
+            num_map[num] = i
         
+        return []
 
         
