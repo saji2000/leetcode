b4848e2e592cdb4849b7dcfc1df5204d7a77832a
saji2000 <sajaddaneshmand79@gmail.com>
2024-02-10 10:37:14 -0500

    palindrome using regex finished, praise God

--- leetcode.py
+++ leetcode.py
@@ -1,10 +1,5 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = s.lower()
-        myString = ""
-        for i in s:
-            if i.isalnum():
-                myString += i
-        if myString == myString[::-1]:
-            return True
-        return False
+        s = re.sub(r'[\W_]', "", s)
+        return s == s[::-1]
