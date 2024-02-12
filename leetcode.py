b0bd138cbced4733c9a29c8f9a6ac558c5fa1d2c
saji2000 <sajaddaneshmand79@gmail.com>
2024-02-12 11:44:48 -0500

    valid parantheses using a map and fifo, praise God

--- leetcode.py
+++ leetcode.py
@@ -5,7 +5,7 @@ class Solution:
 
         for i in s:
             if i in '({[':
-                queue.append()
+                queue.append(i)
             elif len(queue) == 0 or i != tags[queue.pop()]:
                 return False
         return len(queue) == 0
