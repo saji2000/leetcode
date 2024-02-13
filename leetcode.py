f2c698bb364728d06cc311377a235d7e4d6429d1
saji2000 <sajaddaneshmand79@gmail.com>
2024-02-13 12:39:34 -0500

    Reversed linked list does using recursive approach, praise God

--- leetcode.py
+++ leetcode.py
@@ -5,14 +5,12 @@ class ListNode:
         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head:
-            return 
+        if not head or not head.next:
+            return head
         
-        prev = None
-        curr = head
-        while curr:
-            next = curr.next
-            curr.next = prev
-            prev = curr
-            curr = next
-        return prev
+        reversed_list = self.reverseList(head.next)
+
+        head.next.next = head  
+        head.next = None
+
+        return reversed_list
