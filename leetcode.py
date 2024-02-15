dcd639ab6a70e2de29ef18229d33dc42c5e45e27
saji2000 <sajaddaneshmand79@gmail.com>
2024-02-15 12:12:51 -0500

    Has cycle finished using two pointers, fast and slow, praise God

--- leetcode.py
+++ leetcode.py
@@ -1,23 +1,18 @@
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-
-        myList = head = ListNode() 
-
-        while list1 and list2:
-            if list1.val < list2.val:
-                head.next = list1
-                list1 = list1.next
-            else:
-                head.next = list2
-                list2 = list2.next
-            head = head.next
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if not head:
+            return 
         
-        if list1 or list2:
-            head.next = list1 if list1 else list2
+        curr = head
 
-        return myList.next
+        while curr.next and curr.next.next:
+            curr = curr.next.next
+            head = head.next
+            if curr == head:
+                return True
+        return False
+        
+
+        
+
