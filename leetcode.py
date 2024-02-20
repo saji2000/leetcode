f6c05bd0d438aab474dd9972d857d13609caf41c
saji2000 <sajaddaneshmand79@gmail.com>
2024-02-20 10:56:21 -0500

    isSubTree easy solutionfinished

--- leetcode.py
+++ leetcode.py
@@ -5,18 +5,8 @@ class TreeNode:
         self.left = left
         self.right = right
 class Solution:
-    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if p is None or q is None:
-            return p == q
-        
-        if p.val != q.val:
-            return False
-
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
     
     def isSubTree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        if root is None:
-            return False
-        if self.isSameTree(root, subRoot):
+        if str(subRoot) in str(root):
             return True
-        return self.isSubTree(root.left, subRoot.left) and self.isSubTree(root.right, subRoot)
+        return False
