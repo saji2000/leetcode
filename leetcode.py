75b86b217c20cc4c2ac6716cff875e7e6f145176
saji2000 <sajaddaneshmand79@gmail.com>
2024-02-21 09:31:49 -0500

    Climbing stairs using Memoization

--- leetcode.py
+++ leetcode.py
@@ -1,9 +1,14 @@
 class Solution:
+    memo = {}
     def climbStairs(self, n: int) -> int:
         if n == 1:
             return 1
         if n == 2:
             return 2
+        if (n-1) not in self.memo:
+            self.memo[n-1] = self.climbStairs(n-1)
+        if (n-2) not in self.memo:
+            self.memo[n-2] = self.climbStairs(n-2)
         
-        return self.climbStairs(n-1) + self.climbStairs(n-2)
+        return self.memo[n-1] + self.memo[n-2]
         
