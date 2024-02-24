ed5a2b6d2ec3adb580d383a204c495933c33e3ff
saji2000 <sajaddaneshmand79@gmail.com>
2024-02-24 10:18:23 -0500

    hamming weight done by coonverting the number into binary by bin() and then converting it str and using count() method to count all the ones

--- leetcode.py
+++ leetcode.py
@@ -1,12 +1,3 @@
-class Interval:
-    def __init__(self, start, end):
-        self.start = start
-        self.end = end
 class Solution:
-    def canAttendMeetings(intervals):
-        intervals.sort(key=lambda x: x.start)
-    
-        for i in range(len(intervals) - 1):
-            if intervals[i].end > intervals[i+1].start:
-                return False
-        return True
+    def hammingWeight(self, n: int) -> int:
+        return str(bin(n)).count('1')
