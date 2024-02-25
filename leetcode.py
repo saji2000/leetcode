7493ac6651d860e6898939b06e06a4209e728658
saji2000 <sajaddaneshmand79@gmail.com>
2024-02-25 10:12:28 -0500

    Counting bits using string and binary conversion

--- leetcode.py
+++ leetcode.py
@@ -1,10 +1,3 @@
 class Solution:
     def countingBits(self, n: int) -> [int]:
-        ans = []
-        for i in range(0, n + 1):
-            count = 0
-            while i:
-                count += i % 2
-                i = i >> 1
-            ans.append(count)
-        return ans
+        return str(bin(n)).count('1')
