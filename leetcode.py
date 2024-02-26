82cb0f6ce609e0b7bdbadf948967eabea030b5c8
saji2000 <sajaddaneshmand79@gmail.com>
2024-02-26 10:49:24 -0500

    Reverse bits using formating

--- leetcode.py
+++ leetcode.py
@@ -1,3 +1,5 @@
 class Solution:
-    def countingBits(self, n: int) -> [int]:
-        return str(bin(n)).count('1')
+    def reverseBits(self, n: int) -> int:
+        n = format(n, 'b')
+        n = n.zfill(32)
+        return int(n[::-1], 2)
