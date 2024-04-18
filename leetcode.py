51039ced4ecd7cbadf5f073d5e582ee4a5c2ab3b
saji2000 <sajaddaneshmand79@gmail.com>
2024-04-17 21:42:47 -0400

    Valid parantheses started, praise God

--- leetcode.py
+++ leetcode.py
@@ -1,5 +1,2 @@
 class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
-        n = len(nums)
-        missing = (n * (n + 1)) // 2 - sum(nums)
-        return missing
+    def isValid(self, s: str) -> bool:
