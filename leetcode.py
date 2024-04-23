c5ca46d5c195b4a2f96d22136271a6f79a0df71e
saji2000 <sajaddaneshmand79@gmail.com>
2024-04-22 22:11:13 -0400

    is palindrome using regex, praise God

--- leetcode.py
+++ leetcode.py
@@ -1,7 +1,5 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
-        return
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        s = re.sub(r'\W+', '', s)
+        return s == s[::-1]
