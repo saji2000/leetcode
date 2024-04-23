e4439b629d072e7e84ee6889f8c31b7f6dc2e9ac
saji2000 <sajaddaneshmand79@gmail.com>
2024-04-22 21:02:08 -0400

    Contains duplicated done, praise God

--- leetcode.py
+++ leetcode.py
@@ -1,2 +1,8 @@
 class Solution:
-    def isValid(self, s: str) -> bool:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        mySet = set()
+        for i in nums:
+            if i in mySet:
+                return True
+            mySet.add(i)
+        return False
