ec237a3f6dbd252d9e161d858d03b5efb939644e
saji2000 <sajaddaneshmand79@gmail.com>
2024-04-22 22:06:21 -0400

    Two sum using brute force, praise God

--- leetcode.py
+++ leetcode.py
@@ -1,9 +1,7 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        myDict = dict()
-
         for i in range(len(nums)):
-            if target - nums[i] in myDict:
-                return [i, myDict[target - nums[i]]]
-            myDict[nums[i]] = i
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
         return
