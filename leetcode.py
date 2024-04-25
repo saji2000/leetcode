1497ec4fe70ee691aee420feb848ca3c4f429284
saji2000 <sajaddaneshmand79@gmail.com>
2024-04-25 12:49:54 -0400

    same tree using recursion, praise God

--- leetcode.py
+++ leetcode.py
@@ -6,5 +6,7 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        return str(p) == str(q)
+        if not p or not q:
+            return p == q
         
+        return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
