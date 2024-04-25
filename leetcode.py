31d191dbe42d24cba951f7ad55c02f4eadde44ae
saji2000 <sajaddaneshmand79@gmail.com>
2024-04-24 20:41:55 -0400

    merge two lists using recursion, praise God

--- leetcode.py
+++ leetcode.py
@@ -6,20 +6,19 @@ class ListNode:
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
 
-        ans = ListNode()
-        head = ans
-
-        while list1 and list2:
-            if list1.val < list2.val:
-                ans.next = list1
-                list1 = list1.next
-            else:
-                ans.next = list2
-                list2 = list2.next
-            ans = ans.next
-
-        if not list1 and list2:
-            ans.next = list2
-        elif not list2 and list1:
-            ans.next = list1
-        return head.next
+        if not list1 and not list2:
+            return
+        
+        if not list1:
+            return list2
+        elif not list2:
+            return list1
+        
+        if list1.val < list2.val:
+            head = list1
+            head.next = self.mergeTwoLists(list1.next, list2)
+        else:
+            head = list2
+            head.next = self.mergeTwoLists(list1, list2.next)
+        
+        return head
