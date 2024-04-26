30a48e900178ec88e37185f89929c21c3788a9a4
saji2000 <sajaddaneshmand79@gmail.com>
2024-04-25 23:53:11 -0400

    count bits, praise God

--- leetcode.py
+++ leetcode.py
@@ -3,7 +3,13 @@ class Solution:
         count = 0
         while n:
             remainder = n % 2
-            n = n//2
+            n = n // 2
             if remainder:
                 count += 1
         return count
+    def countBits(self, n: int) -> List[int]:
+        ans = [0] * (n + 1)
+        for i in range(len(ans)):
+            ans[i] = self.hammingWeight(i)
+        return ans
+        
