8574f4dc52cff7f979921c00ee5fc98b603c5fe8
saji2000 <sajaddaneshmand79@gmail.com>
2024-04-25 20:03:26 -0400

    sub tree using recursion, praise God

--- leetcode.py
+++ leetcode.py
@@ -5,5 +5,12 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p or not q:
+            return p == q
+        return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+    
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        return str(subRoot) in str(root)
+        if not root:
+            return False
+        return self.isSameTree(root, subRoot) or self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
