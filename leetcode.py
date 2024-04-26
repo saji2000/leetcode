81ca5954eaed8ff53fae65a711c7ee650f935047
saji2000 <sajaddaneshmand79@gmail.com>
2024-04-25 20:11:05 -0400

    climb stairs using brute force, praise God

--- leetcode.py
+++ leetcode.py
@@ -1,16 +1,6 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
-    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if not p or not q:
-            return p == q
-        return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-    
-    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        if not root:
-            return False
-        return self.isSameTree(root, subRoot) or self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+    def climbStairs(self, n: int) -> int:
+        if n == 0 or n == 1 or n == 2:
+            return n
+        
+        return self.climbStairs(n-1) + self.climbStairs(n-2)
