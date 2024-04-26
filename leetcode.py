f73ad77da90f0c8f53fb6dd2c5c18c63cda3ad67
saji2000 <sajaddaneshmand79@gmail.com>
2024-04-25 20:12:42 -0400

    climb stairs using memorization, praise God

--- leetcode.py
+++ leetcode.py
@@ -1,6 +1,12 @@
 class Solution:
+    memo = {}
     def climbStairs(self, n: int) -> int:
         if n == 0 or n == 1 or n == 2:
             return n
-        
-        return self.climbStairs(n-1) + self.climbStairs(n-2)
+
+        if n in self.memo:
+            return self.memo[n]
+        else:
+            self.memo[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
+
+        return self.memo[n]
