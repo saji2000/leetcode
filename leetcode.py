53dcffe7591c58b075c3d71df1e8d9fa8862d6fb
saji2000 <sajaddaneshmand79@gmail.com>
2024-04-27 12:14:28 -0400

    Group Anagrams using dictionaries optimized, praise God

--- leetcode.py
+++ leetcode.py
@@ -5,11 +5,9 @@ class Solution:
         for i in range(len(strs)):
             myStr = strs[i]
             myStr = ''.join(sorted(myStr))
-            if myStr in myDict:
-                myDict[myStr].append(strs[i])
-            else:
+            if myStr not in myDict:
                 myDict[myStr] = []
-                myDict[myStr].append(strs[i])
+            myDict[myStr].append(strs[i])
         for i in myDict.values():
             ans.append(i)
         return ans
