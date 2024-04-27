270e2b8d6f48feb077720a0dbd0a15dcbd56f91a
saji2000 <sajaddaneshmand79@gmail.com>
2024-04-27 13:11:14 -0400

    Top k frequent elements using counter and dictionaries, praise God

--- leetcode.py
+++ leetcode.py
@@ -1,12 +1,6 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        myDict = dict()
-
-        for i in nums:
-            if i in myDict:
-                myDict[i] += 1
-            else:
-                myDict[i] = 1
+        myDict = Counter(nums)
         
         values = sorted(myDict.keys(), key = lambda i: myDict[i], reverse=True)
 
