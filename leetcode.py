84336898b1cd16260bcbafa0601557377b7893b6
saji2000 <sajaddaneshmand79@gmail.com>
2024-04-27 20:53:19 -0400

    Number of islands using dfs, praise God

--- leetcode.py
+++ leetcode.py
@@ -1,7 +1,22 @@
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        myDict = Counter(nums)
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return
         
-        values = sorted(myDict.keys(), key = lambda i: myDict[i], reverse=True)
+        def dfs(i, j):
+            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] != '1':
+                return
+            grid[i][j] = '0'
+            dfs(i - 1, j)
+            dfs(i + 1, j)
+            dfs(i, j - 1)
+            dfs(i, j + 1)
+        
+        num = 0
 
-        return values[:k]
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    num += 1
+                    dfs(i, j)
+        return num
