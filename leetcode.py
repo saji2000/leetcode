f61a96a36380c109eb7a23d825c667320b6f0f93
saji2000 <sajaddaneshmand79@gmail.com>
2024-04-30 14:05:21 -0400

    Encode and decode, praise God

--- leetcode.py
+++ leetcode.py
@@ -1,22 +1,20 @@
 class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return
-        
-        def dfs(i, j):
-            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] != '1':
-                return
-            grid[i][j] = '0'
-            dfs(i - 1, j)
-            dfs(i + 1, j)
-            dfs(i, j - 1)
-            dfs(i, j + 1)
-        
-        num = 0
 
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == '1':
-                    num += 1
-                    dfs(i, j)
-        return num
+    def encode(self, strs: List[str]) -> str:
+        encoded = ""
+        for i in strs:
+            encoded += str(len(i)) + "#" + i 
+        return encoded
+    
+    def decode(self, s: str) -> List[str]:
+        decoded = []
+        i = 0
+
+        while i < len(s):
+            j = i
+            while s[j] != "#":
+                j += 1
+            length = int(str(s[i:j]))
+            decoded.append(s[j + 1 : j + 1 + length])
+            i = j + 1 + length
+        return decoded
