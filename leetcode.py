97cc557f4f468f037887cfbdd04653ae30eceb78
saji2000 <sajaddaneshmand79@gmail.com>
2024-05-03 18:07:51 -0400

    Three sum using two pointers, praise God

--- leetcode.py
+++ leetcode.py
@@ -1,13 +1,23 @@
 class Solution:
-    def longestConsecutive(self, nums: List[int]) -> int:
-        mySet = set(nums)
-        maximumLength = 0
-
-        for n in mySet:
-            if n - 1 not in mySet:
-                length = 1
-                while n + 1 in mySet:
-                    length += 1 
-                    n += 1
-                maximumLength = max(maximumLength, length)
-        return maximumLength
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        nums.sort()
+        for i, n in enumerate(nums):
+            if i > 0 and n == nums[i - 1]:
+                continue
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                threeSum = n + nums[l] + nums[r]
+                if threeSum < 0:
+                    l += 1
+                elif threeSum > 0:
+                    r -= 1
+                else:
+                    ans.append([n, nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    while l < r and nums[l] == nums[l - 1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r + 1]:
+                        r -= 1
+        return ans
