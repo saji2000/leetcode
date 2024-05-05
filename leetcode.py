e7494b870449d1b6e55623b818525a8d6e2c52f4
saji2000 <sajaddaneshmand79@gmail.com>
2024-05-05 11:55:19 -0400

    Longest non repeating substring, praise God

--- leetcode.py
+++ leetcode.py
@@ -1,13 +1,12 @@
 class Solution:
-    def maxArea(self, height: List[int]) -> int:
-        maxArea = 0
-        l, r = 0, len(height) - 1
-
-        while l < r:
-            area = min(height[l], height[r]) * (r - l)
-            maxArea = max(maxArea, area)
-            if height[l] > height[r]:
-                r -= 1
-            else:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        mySet = set()
+        longest = 0
+        l = 0
+        for r in range(len(s)):
+            while s[r] in mySet:
+                mySet.remove(s[l])
                 l += 1
-        return maxArea
+            mySet.add(s[r])
+            longest = max(longest, len(mySet))
+        return longest
