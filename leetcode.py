a7bd047a0428724034ec32363cc4fa0823a4d13a
saji2000 <sajaddaneshmand79@gmail.com>
2024-05-04 20:53:19 -0400

    Max area, praise God

--- leetcode.py
+++ leetcode.py
@@ -1,23 +1,13 @@
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        ans = []
-        nums.sort()
-        for i, n in enumerate(nums):
-            if i > 0 and n == nums[i - 1]:
-                continue
-            l, r = i + 1, len(nums) - 1
-            while l < r:
-                threeSum = n + nums[l] + nums[r]
-                if threeSum < 0:
-                    l += 1
-                elif threeSum > 0:
-                    r -= 1
-                else:
-                    ans.append([n, nums[l], nums[r]])
-                    l += 1
-                    r -= 1
-                    while l < r and nums[l] == nums[l - 1]:
-                        l += 1
-                    while l < r and nums[r] == nums[r + 1]:
-                        r -= 1
-        return ans
+    def maxArea(self, height: List[int]) -> int:
+        maxArea = 0
+        l, r = 0, len(height) - 1
+
+        while l < r:
+            area = min(height[l], height[r]) * (r - l)
+            maxArea = max(maxArea, area)
+            if height[l] > height[r]:
+                r -= 1
+            else:
+                l += 1
+        return maxArea
