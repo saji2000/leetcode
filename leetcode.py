a90dc5ed011604ac44c2c3f8118be3467c8a97ad
saji2000 <sajaddaneshmand79@gmail.com>
2024-05-09 14:13:07 -0400

    find minimum in roated array, praise God

--- leetcode.py
+++ leetcode.py
@@ -1,14 +1,12 @@
 class Solution:
-    def characterReplacement(self, s: str, k: int) -> int:
-        count = {}
-        l = 0
-        res = 0
-        for r in range(len(s)):
-            count[s[r]] = 1  + count.get(s[r], 0)
+    def findMin(self, nums: List[int]) -> int:
+        l, r = 0, len(nums) - 1
 
-            if (r - l + 1) - max(count.values()) > k:
-                count[s[l]] -= 1
-                l += 1
-            
-            res = max(res, r - l + 1)
-        return res
+        while l < r:
+            m = (r + l) // 2
+
+            if nums[m] > nums[r]:
+                l = m + 1
+            else:
+                r = m
+        return nums[l]
