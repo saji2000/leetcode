f83b902cdbe2a85ca0456b8c9824b0b35e9bcdbd
saji2000 <sajaddaneshmand79@gmail.com>
2024-05-22 13:50:06 -0400

    3 sum, praise God

--- leetcode.py
+++ leetcode.py
@@ -18,9 +18,9 @@ class Solution:
                     ans.append([nums[i], nums[l], nums[r]])
                     l += 1
                     r -= 1
-                    while l < r and nums[l] == nums[l + 1]:
+                    while l < r and nums[l] == nums[l - 1]:
                         l += 1
-                    while l < r and nums[r] == nums[r - 1]:
+                    while l < r and nums[r] == nums[r + 1]:
                         r -= 1
         return ans
                 
