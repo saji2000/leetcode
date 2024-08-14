2f767fa446f2dcf6034dfeabe18e60a3d8ed9e96
saji2000 <sajaddaneshmand79@gmail.com>
2024-08-14 19:20:27 -0400

    Delete nodes finished, praise God

--- leetcode.py
+++ leetcode.py
@@ -1,29 +1,20 @@
-class Logger:
-    def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.messages = {}
+class Solution:
+    def delNodes(self, root: Optional[TreeNode], to_delete: List[int]) -> List[TreeNode]:
+        to_delete = set(to_delete)
+        remaining_roots = set([root])
 
-    def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
-        """
-        Returns true if the message should be printed in the given timestamp, otherwise returns false.
-        If this method returns false, the message will not be printed.
-        The timestamp is in seconds granularity.
-        """
-        print(self.messages)
-        if message not in self.messages:
-            self.messages[message] = timestamp
-            return True
-        if  timestamp < self.messages[message] + 10:
-            self.messages[message] = timestamp
-            return False
-        return True
-
-logger = Logger()
-print(logger.shouldPrintMessage(1, "foo")); 
-print(logger.shouldPrintMessage(2, "bar")); 
-print(logger.shouldPrintMessage(3, "foo"));  
-print(logger.shouldPrintMessage(8, "bar"));  
-print(logger.shouldPrintMessage(10, "foo")); 
-print(logger.shouldPrintMessage(11, "foo")); 
+        def dfs(node):
+            if not node:
+                return None
+            
+            res = node
+            if node.val in to_delete:
+                res = None
+                remaining_roots.discard(node)
+                if node.left: remaining_roots.add(node.left)
+                if node.right: remaining_roots.add(node.right)
+            node.left = dfs(node.left)
+            node.right = dfs(node.right)
+            return res
+        dfs(root)
+        return remaining_roots
