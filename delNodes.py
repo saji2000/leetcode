00de772addccdb9273a15b032865b195817f563c
saji2000 <sajaddaneshmand79@gmail.com>
2024-08-17 12:40:21 -0400

    Logger finished, praise God

---
+++ delNodes.py
@@ -0,0 +1,20 @@
+def delNodes(self, root: Optional[TreeNode], to_delete: List[int]) -> List[TreeNode]:
+        to_delete = set(to_delete)
+        remaining_roots = set([root])
+
+        def dfs(node):
+            if not node:
+                return
+            
+            res = node
+            if node.val in to_delete:
+                res = None
+                remaining_roots.discard(node)
+                if node.left: remaining_roots.add(node.left)
+                if node.right: remaining_roots.add(node.right)
+            node.left = dfs(node.left)
+            node.right = dfs(node.right)
+            return res
+        dfs(root)
+
+        return list(remaining_roots)
---
+++ logger.py
@@ -0,0 +1,19 @@
+class Logger:
+    message_times = {}
+    def shouldPrintMessage(self, message, timestamp):
+
+        if message not in self.message_times:
+            self.message_times[message] = timestamp
+            return True
+        if self.message_times[message] + 10 <= timestamp:
+            self.message_times[message] = timestamp
+            return True
+        return False
+    
+logger = Logger()
+print(logger.shouldPrintMessage("foo", 1))
+print(logger.shouldPrintMessage("foo", 2))
+print(logger.shouldPrintMessage("boo", 1))
+print(logger.shouldPrintMessage("foo", 11))
+print(logger.shouldPrintMessage("boo", 10))
+
