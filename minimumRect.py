f887e9f0e3f82f235dbb0b0bd4063f1a9bd9831e
saji2000 <sajaddaneshmand79@gmail.com>
2024-08-18 15:24:52 -0400

    Minimum Rectangle optimized finished, praise God

--- minimumRect.py
+++ minimumRect.py
@@ -2,22 +2,13 @@ from math import inf
 
 class Solution:
     def minAreaRect(self, points: List[List[int]]) -> int:
-        hashmap = {}
-
-        for i in range(len(points)):
-            if points[i][0] not in hashmap:
-                hashmap[points[i][0]] = set()
-            hashmap[points[i][0]].add(points[i][1])
-
+        seen = set()
         minimumArea = inf
-        for i in range(len(points)):
-            for j in range(len(points)):
-                x1, y1 = points[i][0], points[i][1]
-                x2, y2 = points[j][0], points[j][1]
-
-                if x1 != x2 and y1 != y2:
-                    if y1 in hashmap[x2] and y2 in hashmap[x1]:
-                        minimumArea = min(minimumArea, abs(y1 - y2) * abs(x1 - x2))
+        for x1, y1 in points:
+            for x2, y2 in seen:
+                if (x1, y2) in seen and (x2, y1) in seen:
+                    minimumArea = min(minimumArea, abs(x1 - x2) * abs(y1 - y2))
+            seen.add((x1, y1))
         if minimumArea == inf:
             return 0
         return minimumArea
