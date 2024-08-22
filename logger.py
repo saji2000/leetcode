cea6bf249fbf0ad86be82e591b595dfe404146b8
saji2000 <sajaddaneshmand79@gmail.com>
2024-08-22 19:17:57 -0400

    Logger finished, praise God

--- logger.py
+++ logger.py
@@ -1,23 +1,26 @@
 class Logger:
-    message_times = {}
-
+    messageTimes = {}
     def cleanup(self, timestamp):
-        for i in self.message_times.keys():
-            if self.message_times[i] + 10 < timestamp:
-                del self.message_times[i]
-    def shouldPrintMessage(self, message, timestamp):
+        deleteMessages = []
+        for message in self.messageTimes.keys():
+            if self.messageTimes[message] + 10 < timestamp:
+                deleteMessages.append(message)
+        for message in deleteMessages:
+            del self.messageTimes[message]
 
+    def shouldPrintMessage(self, message, timestamp):
         self.cleanup(timestamp)
 
-        if message not in self.message_times or self.message_times[message] + 10 <= timestamp:
-            self.message_times[message] = timestamp
+        if message not in self.messageTimes or self.messageTimes[message] + 10 <= timestamp:
+            self.messageTimes[message] = timestamp
             return True
         return False
-    
+
 logger = Logger()
 print(logger.shouldPrintMessage("foo", 1))
 print(logger.shouldPrintMessage("foo", 2))
 print(logger.shouldPrintMessage("boo", 1))
 print(logger.shouldPrintMessage("foo", 11))
 print(logger.shouldPrintMessage("boo", 10))
+print(logger.shouldPrintMessage("boo", 50))
 
