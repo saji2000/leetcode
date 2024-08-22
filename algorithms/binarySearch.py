39c2741d4bc0c41a7ef642fc9c61e1bce92cdea0
saji2000 <sajaddaneshmand79@gmail.com>
2024-08-22 17:58:13 -0400

    Binary search finished, praise God

--- algorithms/binarySearch.py
+++ algorithms/binarySearch.py
@@ -2,10 +2,9 @@ class Solution:
     def search(self, nums, target) -> int:
         l = 0
         r = len(nums) - 1
+
         while l <= r:
-            mid = (l + r) // 2
-            print("l: " + str(l))
-            print("r: " + str(r))
+            mid = (l + r)//2
             if nums[mid] == target:
                 return mid
             elif nums[mid] < target:
@@ -13,7 +12,5 @@ class Solution:
             else:
                 r = mid - 1
         return -1
-
-
 solution = Solution()
-print(solution.search([0, 1, 5, 7, 9, 12], 12))
+print(solution.search([0, 1, 5, 7, 9, 12], 10))
