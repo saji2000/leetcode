203499976db839cbad916bf640f817e3f11c3752
saji2000 <sajaddaneshmand79@gmail.com>
2024-08-26 16:03:35 -0400

    Difference finished, praise God

--- diff.py
+++ diff.py
@@ -1,27 +1,43 @@
-def obj_diff(obj1, obj2):
-    diff = {}
+def find_differences(obj1, obj2):
+    # If the types of obj1 and obj2 are different, return them as a difference array
+	if type(obj1) != type(obj2):
+		return [obj1, obj2]
 
-    # Collect all unique keys from both dictionaries
-    keys = set(obj1.keys()).union(obj2.keys())
+	# If they are both dictionaries, compare their keys and values recursively
+	if isinstance(obj1, dict) and isinstance(obj2, dict):
+		diff = {}
+		for key in obj1.keys():
+			if key in obj2:
+				nested_diff = find_differences(obj1[key], obj2[key])
+				if nested_diff:
+					diff[key] = nested_diff
+		return diff
 
-    for key in keys:
-        if key in obj1 and key in obj2:
-            # Check if values are different
-            if obj1[key] != obj2[key]:
-                diff[key] = [obj1[key], obj2[key]]
-        elif key in obj1:
-            diff[key] = [obj1[key], None]
-        else:
-            diff[key] = [None, obj2[key]]
+	# If they are both lists, compare their elements recursively
+	if isinstance(obj1, list) and isinstance(obj2, list):
+		diff = []
+		minLength = min(len(obj1), len(obj2))
 
-    return diff
+		for i in range(minLength):
+			nested_diff = find_differences(obj1[i], obj2[i])
+			if nested_diff:
+				diff[str(i)] = nested_diff
+		return diff
 
+	# For primitive values (int, float, str, etc.), compare directly
+	if obj1 != obj2:
+		return [obj1, obj2]
 
-# Example usage
+	# If no differences are found, return an empty object (indicating no differences)
+	return {}
+
+# Example Usage:
+
 obj1 = {
+
   "a": 1,
   "v": 3,
-  "x": [],
+  "x": [5, 6],
   "z": {
     "a": None
   }
@@ -29,10 +45,18 @@ obj1 = {
 obj2 = {
   "a": 2,
   "v": 4,
+  "x": {"t": 2},
   "z": {
     "a": 2
   }
 }
 
-diffs = obj_diff(obj1, obj2)
-print(diffs)
+print(find_differences(obj1, obj2))
+# Output:
+# {
+#   "a": [1, 2],
+#   "v": [3, 4],
+#   "z": {
+#     "a": [None, 2]
+#   }
+# }
