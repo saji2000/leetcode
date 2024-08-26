8657f5cd208aecd6fedf217b62aad924753c0c67
saji2000 <sajaddaneshmand79@gmail.com>
2024-08-26 15:01:36 -0400

    Delete nodes finished finished, praise God

--- delNodes.py
+++ delNodes.py
@@ -1,18 +1,22 @@
 def delNodes(self, root: Optional[TreeNode], to_delete: List[int]) -> List[TreeNode]:
         to_delete = set(to_delete)
         remaining_roots = set([root])
+
         def dfs(node):
             if not node:
                 return
-            res = node
-            if node.val in to_delete:
-                res = None
-                remaining_roots.discard(node)
+            
+            curr = node 
+
+            if curr.val in to_delete:
+                remaining_roots.discard(curr)
+                curr = None
                 if node.left: remaining_roots.add(node.left)
                 if node.right: remaining_roots.add(node.right)
             node.left = dfs(node.left)
             node.right = dfs(node.right)
-            return res
-        dfs(root)
+            return curr
 
+        dfs(root)
         return list(remaining_roots)
+                
