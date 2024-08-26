bf288adf8479941582e9150ccffbec12ea431c47
saji2000 <sajaddaneshmand79@gmail.com>
2024-08-26 15:17:45 -0400

    Minimum rect finished, praise God

--- minimumRect.py
+++ minimumRect.py
@@ -2,12 +2,13 @@ from math import inf
 class Solution:
     def minAreaRect(self, points: List[List[int]]) -> int:
         seen = set()
-        minimumArea = inf
+        area = inf
+
         for x1, y1 in points:
             for x2, y2 in seen:
                 if (x1, y2) in seen and (x2, y1) in seen:
-                    minimumArea = min(minimumArea, abs(x1 - x2) * abs(y1 - y2))
+                    area = min(area, abs(x1 - x2) * abs(y1 - y2))
             seen.add((x1, y1))
-        if minimumArea == inf:
+        if area == inf:
             return 0
-        return minimumArea
+        return area
