3e25f1840d19e5af9de8923096d4d55ecae099fa
saji2000 <sajaddaneshmand79@gmail.com>
2024-08-27 13:30:30 -0400

    Range module finished, praise God

--- rangeModule.py
+++ rangeModule.py
@@ -1,22 +1,42 @@
+import bisect
+
 class RangeModule:
     def __init__(self):
         self.intervals = []
 
     def addRange(self, left: int, right: int) -> None:
-        for i in range(left, right):
-            self.mySet.add(i)
+        bisect.insort(self.intervals, [left, right])
 
+        output = [self.intervals[0]]
+
+        for start, end in self.intervals[1:]:
+            lastEnd = output[-1][1]
+
+            if start <= lastEnd:
+                output[-1][1] = max(end, lastEnd)
+            else:
+                output.append([start, end])
+        
+        self.intervals = output
+        
     def queryRange(self, left: int, right: int) -> bool:
-        for i in range(left, right):
-            if i not in self.mySet:
-                return False
-        return True
+        for start, end in self.intervals:
+            if left >= start and right <= end:
+                return True
+        return False
 
     def removeRange(self, left: int, right: int) -> None:
-        for i in range(left, right):
-            if i in self.mySet:
-                self.mySet.remove(i)
+        output = []
 
+        for start, end in self.intervals:
+            if end <= left or start >= right:
+                output.append([start, end])
+            else:
+                if start < left:
+                    output.append([start, left])
+                if end > right:
+                    output.append([right, end])
+        self.intervals = output
 # Example usage:
 # obj = RangeModule()
 # obj.addRange(left, right)
