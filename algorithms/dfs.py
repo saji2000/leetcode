8edca09b37a244f45bf62bf4724467e634a91bab
saji2000 <sajaddaneshmand79@gmail.com>
2025-01-15 12:58:11 -0500

    dfs finished

--- algorithms/dfs.py
+++ algorithms/dfs.py
@@ -4,19 +4,21 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
 class Solution:
     def recoverTree(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
         temp = []
         def dfs(node):
             if not node:
-                return 
+                return
             dfs(node.left)
             temp.append(node)
             dfs(node.right)
         
         dfs(root)
-
-        sort = sorted(n.val for n in temp)
-        for i in range(len(temp)):
-            temp[i].val = sort[i]
+        sorted_array = sorted(n.val for n in temp)
+        for i in range(len(sorted_array)):
+            temp[i].val = sorted_array[i]
+        
