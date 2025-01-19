74332c1b7ae16d9e009c89517523a46aa6c8dea6
saji2000 <sajaddaneshmand79@gmail.com>
2025-01-19 13:21:07 -0500

    bfs finished

---
+++ bfs.py
@@ -0,0 +1,20 @@
+class Solution:
+    
+    def numIslands(self, grid: List[List[str]]) -> int:
+        count = 0
+        def bfs(i, j):
+            if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]):
+                return
+            if grid[i][j] == "1":
+                grid[i][j] = "0"
+                bfs(i, j + 1)
+                bfs(i + 1, j)
+                bfs(i, j - 1)
+                bfs(i - 1, j)
+        
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):  
+                if grid[i][j] == "1":
+                    count += 1
+                    bfs(i, j)
+        return count
