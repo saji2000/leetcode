3133e1389cf89dca53d6ac0ef94b5f6c143f48bc
saji2000 <sajaddaneshmand79@gmail.com>
2025-01-25 19:17:51 -0500

    is Anagram finished using Counter function

--- leetcode.py
+++ leetcode.py
@@ -1,6 +1,12 @@
 from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        print(Counter(s))
+        print(Counter(t))
         if Counter(s) == Counter(t):
             return True
         return False
+    
+solution = Solution()
+
+print(solution.isAnagram("abc", "bca"))
