942348ed82564d1359fc1d3ead0026139d1222cd
saji2000 <sajaddaneshmand79@gmail.com>
2025-01-25 19:21:27 -0500

    is Anagram finished using sorted

--- leetcode.py
+++ leetcode.py
@@ -1,16 +1,3 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        dictionary_s = {}
-        dictionary_t = {}
-        for i in s:
-            if i not in dictionary_s:
-                dictionary_s[i] = 1
-            else:
-                dictionary_s[i] += 1
-        dictionary_t = {}
-        for i in t:
-            if i not in dictionary_t:
-                dictionary_t[i] = 1
-            else:
-                dictionary_t[i] += 1
-        return dictionary_t == dictionary_s
+        return sorted(s) == sorted(t)
