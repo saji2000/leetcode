bdd0f99f8a0828daf4b9e2b7cd29ae996b953bfd
saji2000 <sajaddaneshmand79@gmail.com>
2025-01-26 11:37:07 -0500

    two sum finished

--- leetcode.py
+++ leetcode.py
@@ -1,3 +1,10 @@
 class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dictionary = {}
+
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in dictionary:
+                return dictionary[complement], i
+            dictionary[nums[i]] = i
+        return
