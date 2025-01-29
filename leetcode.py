11495abbaf730907d87d60986b7f7054a846a67d
saji2000 <sajaddaneshmand79@gmail.com>
2025-01-29 12:45:58 -0500

    top k freq reviewed

--- leetcode.py
+++ leetcode.py
@@ -1,15 +1,19 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        myDict = {}
+        hashmap = {}
 
         for i in nums:
-            if i not in myDict:
-                myDict[i] = 1
+            if i not in hashmap:
+                hashmap[i] = 1
             else:
-                myDict[i] += 1
-        freq = sorted(myDict.items(), key = lambda x: x[1], reverse=True)
+                hashmap[i] += 1
+        
+        ordered = sorted(hashmap.items(), key = lambda x: x[1], reverse=True)
+
         top_k = []
-        for i in freq[:k]:
+
+        for i in ordered[:k]:
             top_k.append(i[0])
+        
         return top_k
         
