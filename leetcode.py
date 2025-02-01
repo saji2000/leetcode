a98ddf5ccc5fbbcba85bb1ded3ba8b758d00abd3
saji2000 <sajaddaneshmand79@gmail.com>
2025-01-31 23:00:17 -0500

    product of array except self started

--- leetcode.py
+++ leetcode.py
@@ -1,24 +1,5 @@
 class Solution:
-
-    def encode(self, strs: list[str]) -> str:
-        encoded = ""
-        for i in strs:
-            encoded += (str(len(i))) + "#" + i
-        return encoded
-
-    def decode(self, s: str) -> list[str]:
-        decoded = []
-        i = 0
-        
-        while i < len(s):
-            pos = s.find('#', i)
-            length = int(s[i:pos])
-            decoded.append(s[pos + 1: pos + 1 + length])
-            i = pos + length + 1
-        return decoded
-
-
-solution = Solution()
-
-encoded = solution.encode(["neet","code","love","you"])
-solution.decode(encoded)
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        result = []
+        for i in range(len(nums)):
+            result[i] = i
