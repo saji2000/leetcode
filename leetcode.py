0f5adb7baf5739578e04b58a6a370619d33ade54
saji2000 <sajaddaneshmand79@gmail.com>
2025-02-05 17:25:51 -0500

    product except self algorithm

--- leetcode.py
+++ leetcode.py
@@ -1,6 +1,15 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        result = []
+        result = [1] * len(nums)
+
+        pre = 1
         for i in range(len(nums)):
-            result[i] = i
-            s = 1
+            result[i] *= pre
+            pre *= nums[i]
+        
+        post = 1
+        for i in reversed(range(len(nums))):
+            result[i] *= post
+            post *= nums[i]
+        
+        return result
