4e62d13c4d651f5f868c8a57a685cfec2dd92eb4
saji2000 <sajaddaneshmand79@gmail.com>
2025-02-11 15:24:31 -0500

    longest consecutive finished, praise God

--- leetcode.py
+++ leetcode.py
@@ -1,18 +1,14 @@
-from collections import defaultdict
 class Solution:
-    def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = defaultdict(set)
-        cols = defaultdict(set)
-        square = defaultdict(set)
-
-        for r in range(9):
-            for c in range(9):
-                if board[r][c] == '.':
-                    continue
-                if board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in square[(r//3, c//3)]:
-                    return False
-                rows[r].add(board[r][c])
-                cols[c].add(board[r][c])
-                square[(r//3, c//3)].add(board[r][c])
+    def longestConsecutive(self, nums: List[int]) -> int:
+        mySet = set(nums)
+        maxLength = 0
+        for i in nums:
+            if i - 1 not in mySet:
+                ptr = i
+                length = 1
+                while ptr + 1 in mySet:
+                    length += 1
+                    ptr += 1
+                maxLength = max(maxLength, length)
+        return maxLength
         
-        return True
