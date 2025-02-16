25071d3c47cda8862158de31b53c46ec3178a12d
saji2000 <sajaddaneshmand79@gmail.com>
2025-02-15 21:17:04 -0500

    Valid palindrome without alnum, praise God

--- leetcode.py
+++ leetcode.py
@@ -1,9 +1,5 @@
+import re
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        myString = ""
-
-        for i in s:
-            if i.isalnum():
-                myString += i
-        myString = myString.lower()
-        return myString == myString[::-1]
+        s = re.sub('[^a-zA-Z0-9]', '', s).lower()
+        return s == s[::-1]
