387ea926f7a689af97f9caa9781155c6156c6102
saji2000 <sajaddaneshmand79@gmail.com>
2025-02-16 18:08:32 -0500

    binary search finished, praise God

--- leetcode.py
+++ leetcode.py
@@ -1,5 +1,13 @@
-import re
 class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        s = re.sub('[^a-zA-Z0-9]', '', s).lower()
-        return s == s[::-1]
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            mid = (l + r)//2
+            if nums[mid] > target:
+                r = mid - 1
+            elif nums[mid] < target:
+                l = mid + 1
+            else:
+                return mid
+        return -1
