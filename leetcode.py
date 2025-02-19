12c616c36807a250a53b4d2edc3f6dd4cf332495
saji2000 <sajaddaneshmand79@gmail.com>
2025-02-19 13:37:56 -0500

    merge two linked lists, praise God

--- leetcode.py
+++ leetcode.py
@@ -1,17 +1,25 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        head = ListNode()
         node = head
-        prev = None
 
-        while node:
-            nextNode = node.next
-            node.next = prev
-            prev = node
-            node = nextNode
-        
-        return prev
+        while list1 and list2:
+            if list1.val > list2.val:
+                node.next = list2
+                list2 = list2.next
+            else:
+                node.next = list1
+                list1 = list1.next
+            node = node.next
+
+        if list1:
+            node.next = list1
+        elif list2:
+            node.next = list2
+
+        return head.next
