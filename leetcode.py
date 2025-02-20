a31fc4290369f93280c8e9f2d4a1c71aaa73af6e
saji2000 <sajaddaneshmand79@gmail.com>
2025-02-20 13:20:51 -0500

    Has cycle finished, praise God

--- leetcode.py
+++ leetcode.py
@@ -1,25 +1,21 @@
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        head = ListNode()
-        node = head
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if not head or not head.next:
+            return False
 
-        while list1 and list2:
-            if list1.val > list2.val:
-                node.next = list2
-                list2 = list2.next
-            else:
-                node.next = list1
-                list1 = list1.next
-            node = node.next
+        slow = head
+        fast = slow.next
 
-        if list1:
-            node.next = list1
-        elif list2:
-            node.next = list2
-
-        return head.next
+        while fast and fast.next:
+            if slow == fast:
+                return True
+            slow = slow.next
+            fast = fast.next.next
+        return False
+        
