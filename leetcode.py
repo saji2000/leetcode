9fa5123305cfee6ff3e6395fb2610f6718285ca9
saji2000 <sajaddaneshmand79@gmail.com>
2025-02-24 19:59:38 -0500

    Invert tree finished, praise God

--- leetcode.py
+++ leetcode.py
@@ -1,37 +1,15 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
-#         self.next = next
+#         self.left = left
+#         self.right = right
 class Solution:
-    def reorderList(self, head: Optional[ListNode]) -> None:
-        """
-        Do not return anything, modify head in-place instead.
-        """
-        slow, fast = head, head.next
-
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return 
+        right = root.right
+        root.right = self.invertTree(root.left)
+        root.left = self.invertTree(right)
         
-        second = slow.next
-        slow.next = None
-
-        prev = None
-        while second:
-            temp = second.next
-            second.next  = prev
-            prev = second
-            second = temp
-        
-        first = head
-        second = prev
-
-        while first and second:
-            temp1, temp2 = first.next, second.next
-            first.next = second
-            second.next = temp1
-            first = temp1
-            second = temp2
-        
-        return head
+        return root
