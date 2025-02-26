af3777cce072d6b5abfeffb5363e1709680373c8
saji2000 <sajaddaneshmand79@gmail.com>
2025-02-26 18:58:34 -0500

    3sum finished, praise God

--- leetcode.py
+++ leetcode.py
@@ -1,16 +1,22 @@
 class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        l, r = 0, len(numbers) - 1
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
 
-        while l < r:
-            summation = numbers[l] + numbers[r]
+        for i, a in enumerate(nums):
+            if i > 0 and a == nums[i-1]:
+                continue
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                complement = nums[l] + nums[r] + a
+                if complement < 0:
+                    l += 1
+                elif complement > 0:
+                    r -= 1
+                else:
+                    res.append([a, nums[l], nums[r]])
+                    l += 1
+                    while nums[l] == nums[l-1] and l < r:
+                        l += 1
 
-            if summation < target:
-                l += 1
-            elif summation > target:
-                r -= 1
-            else:
-                return [l + 1, r + 1]
-        
-        return 
-        
+        return res
