cf2187c0a35fd7eabdd22ea9c8e03cee81a25134
saji2000 <sajaddaneshmand79@gmail.com>
2025-03-02 12:03:21 -0500

    Maximum Area reviewed, praise God

--- leetcode.py
+++ leetcode.py
@@ -2,6 +2,7 @@ class Solution:
     def maxArea(self, height: List[int]) -> int:
         l, r = 0, len(height) - 1
         maximumArea = 0
+
         while l < r:
             area = (r - l) * min(height[l], height[r])
             maximumArea = max(maximumArea, area)
@@ -9,4 +10,5 @@ class Solution:
                 l += 1
             else:
                 r -= 1
+                
         return maximumArea
