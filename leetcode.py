5fc6fc812989b9873bc52a32adeb94309689479e
saji2000 <sajaddaneshmand79@gmail.com>
2025-03-05 15:41:03 -0500

    Evaluation, praise God

--- leetcode.py
+++ leetcode.py
@@ -1,31 +1,20 @@
-# import numpy as np
-
-class MinStack:
-
-    def __init__(self):
-        self.stack = []
-        self.minStack = []
-
-    def push(self, val: int) -> None:
-        self.stack.append(val)
-        if self.minStack:
-            self.minStack.append(min(self.minStack[-1], val))
-        else:
-            self.minStack.append(val)
-
-    def pop(self) -> None:
-        del self.stack[-1]
-        del self.minStack[-1]
-
-    def top(self) -> int:
-        return self.stack[-1]
-
-    def getMin(self) -> int:
-        return self.minStack[-1]
-
-# Your MinStack object will be instantiated and called as such:
-# obj = MinStack()
-# obj.push(val)
-# obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for i in tokens:
+            try:
+                stack.append(int(i))
+            except:
+                num2 = stack.pop()
+                num1 = stack.pop()
+                if i == '+':
+                    result = num1 + num2
+                elif i == '-':
+                    result = num1 - num2
+                elif i == '/':
+                    result = int(num1 / num2)
+                elif i == '*':
+                    result = num1 * num2
+                stack.append(result)
+                print(result)
+        return stack.pop()
