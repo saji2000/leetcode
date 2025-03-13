82e13edaf329916055b6a8d3da0becbf768c2be2
saji2000 <sajaddaneshmand79@gmail.com>
2025-03-13 13:58:24 -0400

    Daily temperatures reviewed, praise God

--- leetcode.py
+++ leetcode.py
@@ -1,11 +1,11 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        ans = [0] * len(temperatures)
         stack = []
+        ans = [0] * len(temperatures)
 
         for i, t in enumerate(temperatures):
             while stack and t > stack[-1][0]:
                 stackIndex = stack.pop()[1]
-                ans[stackIndex] = (i - stackIndex)
+                ans[stackIndex] = i - stackIndex
             stack.append([t, i])
         return ans
