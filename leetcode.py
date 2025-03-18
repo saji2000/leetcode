f77ee3cb5de8f4ca8433d1c9b861a5ce7fae63c1
saji2000 <sajaddaneshmand79@gmail.com>
2025-03-18 12:40:21 -0400

    Koko eating banana solved, praise God

--- leetcode.py
+++ leetcode.py
@@ -1,27 +1,20 @@
 class Solution:
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        l, r = 0, len(matrix) - 1
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l, r = 1, max(piles)
+        res = r
 
         while l <= r:
-            mid = (l + r)//2
-            if matrix[mid][0] < target and matrix[mid][-1] >= target:
-                break
-            elif matrix[mid][0] < target:
-                l = mid + 1
-            elif matrix[mid][0] > target:
-                r = mid - 1
-            else:
-                return True
-        
-        row = mid
-        l, r = 0, len(matrix[0]) - 1
-        while l <= r:
-            mid = (l + r)//2
 
-            if matrix[row][mid] > target:
-                r = mid - 1
-            elif matrix[row][mid] < target:
-                l = mid + 1
+            k = (l + r)//2
+            hours = 0
+
+            for p in piles:
+                hours += math.ceil(p / k)
+
+            if hours <= h:
+                res = min(res, k)
+                r = k - 1
             else:
-                return True
-        return False
+                l = k + 1
+
+        return res
