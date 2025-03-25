1d5b46c847f8e695a62cd2b05d792182f969dd74
saji2000 <sajaddaneshmand79@gmail.com>
2025-03-25 13:46:52 -0400

    Searching rotated array reviewed, praise God

--- leetcode.py
+++ leetcode.py
@@ -3,21 +3,20 @@ class Solution:
         l, r = 0, len(nums) - 1
 
         while l <= r:
-            mid = (l + r) // 2
+            mid = (l + r)//2
 
             if nums[mid] == target:
                 return mid
-
-            if nums[l] <= nums[mid]:
+            
+            if nums[mid] >= nums[l]:
                 if nums[l] <= target <= nums[mid]:
-                    r = mid - 1
-                else: 
+                    r = mid
+                else:
                     l = mid + 1
-
-            else:  
-                if nums[mid] < target <= nums[r]: 
-                    l = mid + 1
+            else:
+                if nums[mid] <= target <= nums[r]:
+                    l = mid
                 else:
                     r = mid - 1
-
+        
         return -1
