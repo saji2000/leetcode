3214c5242bffcc35820d8f5168c2049c40d25d4a
saji2000 <sajaddaneshmand79@gmail.com>
2025-03-26 09:15:33 -0400

    Binary search for timemap, praise God

--- leetcode.py
+++ leetcode.py
@@ -1,22 +1,36 @@
-class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        l, r = 0, len(nums) - 1
+class TimeMap:
 
+    def __init__(self):
+        self.hashMap = {}
+        
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.hashMap:
+            self.hashMap[key] = []
+        self.hashMap[key].append([value, timestamp])
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key in self.hashMap:
+            values = self.hashMap[key]
+        else:
+            return ""
+        l, r = 0, len(values) - 1
+        res = ""
         while l <= r:
             mid = (l + r)//2
 
-            if nums[mid] == target:
-                return mid
+            if values[mid][1] == timestamp:
+                return values[mid][0]
             
-            if nums[mid] >= nums[l]:
-                if nums[l] <= target <= nums[mid]:
-                    r = mid
-                else:
-                    l = mid + 1
+            if values[mid][1] > timestamp:
+                r = mid - 1
             else:
-                if nums[mid] <= target <= nums[r]:
-                    l = mid
-                else:
-                    r = mid - 1
-        
-        return -1
+                res = values[mid][0]
+                l = mid + 1
+        return res
+
+
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)
