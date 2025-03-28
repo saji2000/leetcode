e9461dad8895e60d3886e8476d3b422d208584fa
saji2000 <sajaddaneshmand79@gmail.com>
2025-03-28 15:41:57 -0400

    Longest substring, praise God

--- leetcode.py
+++ leetcode.py
@@ -1,33 +1,12 @@
-class TimeMap:
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        l, r = 0, 0
+        maxLength = 0
 
-    def __init__(self):
-        self.hashMap = {}
-        
+        while r <= len(s) - 1:
+            while s[r] in s[l:r]:
+                l += 1
+            maxLength = max(maxLength, len(s[l:r+1]))
+            r += 1
+        return maxLength
 
-    def set(self, key: str, value: str, timestamp: int) -> None:
-        if key not in self.hashMap:
-            self.hashMap[key] = []
-        self.hashMap[key].append([value, timestamp])
-
-    def get(self, key: str, timestamp: int) -> str:
-        if key not in self.hashMap:
-            return ""
-
-        res = ""
-        l, r = 0, len(self.hashMap[key]) - 1
-
-        values = self.hashMap[key]
-
-        while l <= r:
-            mid = (l + r)//2
-
-            if values[mid][1] == timestamp:
-                return values[mid][0]
-            
-            if values[mid][1] < timestamp:
-                res = values[mid][0]
-                l = mid + 1
-            else:
-                r = mid - 1
-        
-        return res
