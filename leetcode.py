82bf96c31fbc36ef7b1fd99057d429c23ef5aa8b
saji2000 <sajaddaneshmand79@gmail.com>
2025-03-30 15:54:40 -0400

    Longest substring reviewed, praise God

--- leetcode.py
+++ leetcode.py
@@ -2,7 +2,8 @@ class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         l, r = 0, 0
         maxLength = 0
-        while r <= len(s) - 1:
+
+        while r < len(s):
             while s[r] in s[l:r]:
                 l += 1
             maxLength = max(maxLength, len(s[l:r + 1]))
