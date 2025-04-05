030896dcf7d00a7fe18758c9c9bd255d4459e814
saji2000 <sajaddaneshmand79@gmail.com>
2025-04-05 14:19:39 -0400

    Add two numbers, praise God

--- leetcode.py
+++ leetcode.py
@@ -1,30 +1,31 @@
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
-        self.val = int(x)
-        self.next = next
-        self.random = random
-"""
-
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        if not head:
-            return 
-            
-        hashMap = {}
-        node = head
-        
-        while node:
-            hashMap[node] = ListNode(node.val)
-            node = node.next
-        
-        node = head
-        while node:
-            hashMap[node].next = hashMap.get(node.next)
-            hashMap[node].random = hashMap.get(node.random)
-            node = node.next
-        
-        return hashMap[head]
-        
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
 
+        curr = ListNode(0)
+        head = curr
+        carry = 0
+
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+
+            total = val1 + val2 + carry
+
+            carry = total // 10
+
+            curr.next = ListNode(total % 10)
+
+            curr = curr.next
+
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+        
+        return head.next
+        
