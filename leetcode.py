4ceb8df8067a43294db4deb6c131b06d6b0a479d
saji2000 <sajaddaneshmand79@gmail.com>
2025-04-08 14:26:39 -0400

    LRU cache

--- leetcode.py
+++ leetcode.py
@@ -1,8 +1,40 @@
-class Solution:
-    def findDuplicate(self, nums: List[int]) -> int:
-        mySet = set()
+class LRUCache:
 
-        for i in nums:
-            if i in mySet:
-                return i
-            mySet.add(i)
+    def __init__(self, capacity: int):
+        self.cache = {}
+        self.timeAccessed = {}
+        self.time = 0
+        self.capacity = capacity
+        
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        self.cache["MRU"] = key
+        return self.cache[key]
+        
+
+    def put(self, key: int, value: int) -> None:
+        if len(self.cache) < capacity + 1:
+            self.cache[key] = value
+        
+        else:
+            print("here")
+            MRU = self.cache["MRU"]
+            del self.cache["MRU"]
+            keys = self.cache.keys()
+
+            for i in self.cache.keys():
+                if i != MRU:
+                    LRU = i
+            del self.cache[LRU]
+            self.cache[key] = value
+            self.cache["MRU"] = key
+            self.timeAccessed[key] = self.time
+        self.time += 1
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
