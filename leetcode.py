ffb073c55fd9775b543e20f18cc8885073cf3321
saji2000 <sajaddaneshmand79@gmail.com>
2025-04-07 23:21:31 -0400

    Find duplicate finished, praise God

--- leetcode.py
+++ leetcode.py
@@ -1,30 +1,8 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        carry = 0
-        head = ListNode()
-        node = head
+    def findDuplicate(self, nums: List[int]) -> int:
+        mySet = set()
 
-        while l1 or l2 or carry:
-            val1 = l1.val if l1 else 0
-            val2 = l2.val if l2 else 0
-
-            total = val1 + val2 + carry
-
-            carry = total // 10
-
-            node.next = ListNode(total % 10)
-            
-            node = node.next
-
-            if l1:
-                l1 = l1.next
-            if l2:
-                l2 = l2.next
-
-            
-        return head.next
+        for i in nums:
+            if i in mySet:
+                return i
+            mySet.add(i)
