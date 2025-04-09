75cd4615174747b111c009ca3c57435f2bee8a19
saji2000 <sajaddaneshmand79@gmail.com>
2025-04-09 09:20:28 -0400

    Finished LRU using OrderedDict from collections reviewed

--- leetcode.py
+++ leetcode.py
@@ -9,21 +9,19 @@ class LRUCache:
     def get(self, key: int) -> int:
         if key not in self.cache:
             return -1
-            
+        
         value = self.cache.pop(key)
         self.cache[key] = value
+        return value
 
-        return self.cache[key]
-        
 
     def put(self, key: int, value: int) -> None:
-
-        if key in self.cache:
-            self.cache.pop(key)
-
-        elif len(self.cache) >= self.capacity:
+        if key not in self.cache and len(self.cache) >= self.capacity:
             self.cache.popitem(last=False)
 
+        elif key in self.cache:
+            self.cache.pop(key)
+            
         self.cache[key] = value
 
 
