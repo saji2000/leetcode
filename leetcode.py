1915bcd6ceb912ab6a6461462ed51b8336e8f250
saji2000 <sajaddaneshmand79@gmail.com>
2025-04-10 09:20:23 -0400

    invert binary tree finished

--- leetcode.py
+++ leetcode.py
@@ -1,32 +1,18 @@
-from collections import OrderedDict
-
-class LRUCache:
-
-    def __init__(self, capacity: int):
-        self.cache = OrderedDict()
-        self.capacity = capacity
-
-    def get(self, key: int) -> int:
-        if key not in self.cache:
-            return -1
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return
         
-        value = self.cache.pop(key)
-        self.cache[key] = value
-        return value
+        right = self.invertTree(root.left)
+        left = self.invertTree(root.right)
+        root.left = left
+        root.right = right
 
-
-    def put(self, key: int, value: int) -> None:
-        if key not in self.cache and len(self.cache) >= self.capacity:
-            self.cache.popitem(last=False)
-
-        elif key in self.cache:
-            self.cache.pop(key)
-            
-        self.cache[key] = value
-
-
-
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
+        return root
+        
