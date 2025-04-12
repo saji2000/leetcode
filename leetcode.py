01d3da1173dace981e9df41a6d40c57d3f1cee38
saji2000 <sajaddaneshmand79@gmail.com>
2025-04-12 12:57:39 -0400

    maximum depth in a binary tree finished, praise God

--- leetcode.py
+++ leetcode.py
@@ -1,32 +1,15 @@
-from collections import OrderedDict
-
-class LRUCache:
-
-    def __init__(self, capacity: int):
-        self.cache = OrderedDict()
-        self.capacity = capacity
-
-    def get(self, key: int) -> int:
-        if key not in self.cache:
-            return -1
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
         
-        value = self.cache.pop(key)
-        self.cache[key] = value
-        return value
+        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
 
 
-    def put(self, key: int, value: int) -> None:
-        if key not in self.cache and len(self.cache) >= self.capacity:
-            self.cache.popitem(last=False)
-
-        elif key in self.cache:
-            self.cache.pop(key)
-            
-        self.cache[key] = value
-
-
-
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
+        
