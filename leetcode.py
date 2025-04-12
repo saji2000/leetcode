2c0f554102e1ddb44f40661000624ce328d2e015
saji2000 <sajaddaneshmand79@gmail.com>
2025-04-12 13:03:52 -0400

    maximum depth in a binary tree finished, praise God

--- leetcode.py
+++ leetcode.py
@@ -5,14 +5,9 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
-            return
+            return 0
         
-        right = self.invertTree(root.left)
-        left = self.invertTree(root.right)
-        root.left = left
-        root.right = right
-
-        return root
+        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
         
