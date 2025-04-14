9e712e6fbde3d5262f98470d0e29e093c8a1a7ec
saji2000 <sajaddaneshmand79@gmail.com>
2025-04-14 09:38:39 -0400

    diameter of a binary tree

--- leetcode.py
+++ leetcode.py
@@ -5,9 +5,24 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
+
+    def __init__(self):
+        self.max_diameter = 0
+
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+
+        def height(node):
+            if not node:
+                return 0
+            
+            left = height(node.left)
+            right = height(node.right)
+
+            self.max_diameter = max(self.max_diameter, left + right)
+
+            return max(left, right) + 1
         
-        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+        height(root)
+
+        return self.max_diameter
         
