e118501870ce4ea279f5041c50ee8f9fbcc92c78
saji2000 <sajaddaneshmand79@gmail.com>
2025-04-28 17:57:59 -0400

    level order

--- leetcode.py
+++ leetcode.py
@@ -1,18 +1,34 @@
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+import collections
 
 class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        while root:
-            if root.val < p.val and root.val < q.val:
-                root = root.right
-            elif root.val > p.val and root.val > q.val:
-                root = root.left
-            else:
-                return root
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        res = []
+
+        if not root:
+            return res
         
-        
+        q = collections.deque()
+        q.append(root)
+
+        while q:
+            level_size = len(q)
+            same_level = []
+            print(same_level)
+            for i in range(level_size):
+                node = q.popleft()
+                same_level.append(node.val)
+
+                if node.left:
+                    q.append(node.left)
+
+                if node.right:
+                    q.append(node.right)
+            
+            res.append(same_level)
+        return res
