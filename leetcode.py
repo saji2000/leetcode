bd7d42cfe260fa5722aa116fab912a15508031bc
saji2000 <sajaddaneshmand79@gmail.com>
2025-05-05 09:42:00 -0400

    working on valid bst

--- leetcode.py
+++ leetcode.py
@@ -5,18 +5,31 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def goodNodes(self, root: TreeNode) -> int:
-        def dfs(node, maximum):
-            if not node:
-                return 0
-            
-            if node.val >= maximum:
-                good = 1
-            else:
-                good = 0
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        minimum = 100000
+        maximum = -100000
+        if root.left:
+            maximum = root.left.val
+        
+        if root.right:
+            minimum = root.right.val
 
-            maximum = max(maximum, node.val)
+        if not root:
+            return True
+        
+        if root.left and root.right:
+            if root.left.val > root.right.val:
+                return False
+        
+        if root.right:
+            minimum = min(root.right.val, minimum) 
+            return root.right.val > root.val and root.right.val > maximum
 
-            return good + dfs(node.right, maximum) + dfs(node.left, maximum)
+        if root.left:
+            maximum = max(root.left.val, maximum)
+            return root.left.val < root.val and root.left.val < minimum
 
-        return dfs(root, -10000)
+        left = self.isValidBST(root.left)
+        right = self.isValidBST(root.right)
+        
+        return left and right
