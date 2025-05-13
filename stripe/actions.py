61d231de66f457cf6cee1b5690db9955de1f6cf2
saji2000 <sajaddaneshmand79@gmail.com>
2025-05-13 15:23:21 -0400

    log analyzer finished

--- stripe/actions.py
+++ stripe/actions.py
@@ -1,11 +1,30 @@
 import collections
 class LogAnalyzer:
     def __init__(self):
-        logs_dict = collections.defaultdict(set)
+        self.logs_dict = collections.defaultdict(set)
     
     def process_logs(self, input_str):
         logs = input_str.split(',')
         for log in logs:
-            info 
+            info = log.split(':')
+            time = info[0] + info[1] + info[2]
+            key = info[3] + info[4] + info[5]
+            print(time)
+            self.logs_dict[key].add(time)
+        # print(self.logs_dict)
+        return
+            
+    def get_action_count(self, user, action, resource):
+        # print(self.logs_dict)
+        key = user + action + resource
 
+        if key in self.logs_dict:
+            return len(self.logs_dict[key])
+        
+        return 0
 
+analyzer = LogAnalyzer()
+analyzer.process_logs("2023-10-01T10:00:00:Alice:read:document1,2023-10-01T10:01:00:Bob:write:document2,2023-10-01T10:02:00:Alice:read:document1")
+print(analyzer.get_action_count("Alice", "read", "document1"))  # Output: 2
+print(analyzer.get_action_count("Bob", "write", "document2"))  # Output: 1
+print(analyzer.get_action_count("Alice", "write", "document1"))  # Output: 0
