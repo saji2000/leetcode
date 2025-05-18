46f3531252b0301a6fa8d71aa0f1fcedb5268f2f
saji2000 <sajaddaneshmand79@gmail.com>
2025-05-18 11:26:16 -0400

    kth smallest value in a binary tree optimized

--- leetcode.py
+++ leetcode.py
@@ -6,17 +6,18 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        node = root
         stack = []
         count = 0
+        node = root
 
-        while stack or node:
+        while node or stack:
             while node:
                 stack.append(node)
                 node = node.left
-            
             node = stack.pop()
             count += 1
             if count == k:
                 return node.val
             node = node.right
+        
+            
