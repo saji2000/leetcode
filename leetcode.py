33419e0b5b5fe34ae434a8253bec11a3c43f03b7
saji2000 <sajaddaneshmand79@gmail.com>
2025-10-07 20:23:44 -0400

    is Anagram

--- leetcode.py
+++ leetcode.py
@@ -1,10 +1,5 @@
 class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-
-        my_set = set()
-
-        for n in nums:
-            if n in my_set:
-                return True
-            my_set.add(n)    
+    def isAnagram(self, s: str, t: str) -> bool:
+        if Counter(s) == Counter(t):
+            return True
         return False
