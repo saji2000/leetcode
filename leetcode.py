a224e1df648ccd32c47585b283bd832184045a60
saji2000 <sajaddaneshmand79@gmail.com>
2025-10-07 20:21:16 -0400

    contains duplicate

--- leetcode.py
+++ leetcode.py
@@ -1,22 +1,10 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
-    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        stack = []
-        count = 0
-        node = root
-        while node or stack:
-            while node:
-                stack.append(node)
-                node = node.left
-            node = stack.pop()
-            count += 1
-            if count == k:
-                return node.val
-            node = node.right
-        
-            
+    def containsDuplicate(self, nums: List[int]) -> bool:
+
+        my_set = set()
+
+        for n in nums:
+            if n in my_set:
+                return True
+            my_set.add(n)    
+        return False
