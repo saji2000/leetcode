4b451457ce4658758aa8b2578838302aea4f038d
saji2000 <sajaddaneshmand79@gmail.com>
2025-10-12 14:09:37 -0400

    top k frequent review

--- leetcode.py
+++ leetcode.py
@@ -1,13 +1,7 @@
-from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         freqs = Counter(nums)
 
-        ordered = sorted(freqs.items(), key = lambda x: x[1], reverse=True)
+        ordered = sorted(freqs.keys(), key=lambda x: freqs[x], reverse=True)
 
-        top_k = []
-
-        for i in ordered:
-            top_k.append(i[0])
-        
-        return top_k[0:k]
+        return ordered[0:k]
