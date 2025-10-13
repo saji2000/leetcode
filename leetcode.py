ead66c0ae893f6c45f649aef6de0bbce60588be1
saji2000 <sajaddaneshmand79@gmail.com>
2025-10-13 15:19:38 -0400

    Encode and Decode

--- leetcode.py
+++ leetcode.py
@@ -1,7 +1,36 @@
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        freqs = Counter(nums)
 
-        ordered = sorted(freqs.keys(), key=lambda x: freqs[x], reverse=True)
+    def encode(self, strs: List[str]) -> str:
+        encoded = ""
 
-        return ordered[0:k]
+        for s in strs:
+            encoded += str(len(s)) + "#"
+            encoded += (s)
+        
+        return encoded
+
+    def decode(self, s: str) -> List[str]:
+
+        print(s)
+
+        decoded = []
+
+        i = 0
+
+        while i < len(s):
+
+            j = i
+
+            while s[j] != "#":
+                j += 1
+
+            length = int(s[i:j])
+
+            word_start = j + 1
+            word_end = j + length + 1
+            decoded.append(s[word_start:word_end])
+
+            i = word_end
+    
+        return decoded
+
