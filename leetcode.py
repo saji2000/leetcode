d9c90e18c048f53902eee268bee1efb8c1e63cff
saji2000 <sajaddaneshmand79@gmail.com>
2025-10-15 19:50:45 -0400

    product except self

--- leetcode.py
+++ leetcode.py
@@ -1,33 +1,15 @@
 class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        ans = [1] * len(nums)
 
-    def encode(self, strs: List[str]) -> str:
-        encoded = ""
+        prev = 1
+        for i in range(len(nums)):
+            ans[i] = prev
+            prev *= nums[i]
 
-        for s in strs:
-            encoded += (str(len(s)) + "#")
-            encoded += (s)
-        return encoded
+        post = 1
+        for i in reversed(range(len(nums))):
+            ans[i] *= post
+            post *= nums[i]
 
-    def decode(self, s: str) -> List[str]:
-
-        decoded = []
-        i = 0
-
-        while i < len(s):
-            
-            j = i
-
-            while s[j] != "#":
-                j += 1
-
-            length = int(s[i:j])
-
-            word_start = j + 1
-            word_end = word_start + length
-
-            decoded.append(s[word_start:word_end])
-
-            i = word_end
-
-
-        return decoded
+        return ans
