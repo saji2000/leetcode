ba4a616cdb7ec7f7580dd15396b3f8f5b38def59
saji2000 <sajaddaneshmand79@gmail.com>
2025-10-23 13:36:16 -0400

    daily temps

--- leetcode.py
+++ leetcode.py
@@ -1,13 +1,12 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         n = len(temperatures)
-        res = [0] * n
         stack = []
+        res = [0] * n
 
         for i in range(n):
             while stack and temperatures[i] > temperatures[stack[-1]]:
                 prev_i = stack.pop()
                 res[prev_i] = i - prev_i
             stack.append(i)
-
         return res
