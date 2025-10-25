ad1f500be8eb98903e3cad75edd7f49ece69a607
saji2000 <sajaddaneshmand79@gmail.com>
2025-10-24 22:13:07 -0400

    car fleet

--- leetcode.py
+++ leetcode.py
@@ -1,18 +1,12 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         cars = sorted(zip(position, speed), reverse=True)
-
         fleet = 0
-        ahead_t = 0.0
+        prev_t = 0.0
 
         for pos, speed in cars:
-            
-            time = (target - pos)/speed
-
-            if ahead_t < time:
+            time = (target - pos) / speed
+            if prev_t < time:
                 fleet += 1
-                ahead_t = time
-
-
+                prev_t = time
         return fleet
-                
