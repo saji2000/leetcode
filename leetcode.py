f2d2e3a9dbc9b789d46366123cb2bc5a0e627e67
saji2000 <sajaddaneshmand79@gmail.com>
2025-10-28 15:48:01 -0400

    search 2d matrix

--- leetcode.py
+++ leetcode.py
@@ -1,16 +1,49 @@
+from typing import List
+
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        l, r = 0, len(nums) - 1
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix or not matrix[0]:
+            return False
+        
+        rows = len(matrix)
+        cols = len(matrix[0])
+
+        l, r = 0, rows - 1
+        row = -1
 
         while l <= r:
             mid = (l + r) // 2
 
-            num = nums[mid]
+            row_start = matrix[mid][0]
+            row_end = matrix[mid][cols - 1]
+
+            if target < row_start:
+                r = mid - 1
+            
+            elif target > row_end:
+                l = mid + 1
+            
+            else:
+                row = mid
+                break
+
+        if row == -1:
+            return False
+
+
+        l, r = 0, cols - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+
+            num = matrix[row][mid]
 
             if num > target:
                 r = mid - 1
+            
             elif num < target:
                 l = mid + 1
+            
             else:
-                return mid
-        return -1
+                return True
+        return False
