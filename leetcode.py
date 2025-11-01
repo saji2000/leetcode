c9baeb33defe8ad6f029a4a3a649a76d199c768c
saji2000 <sajaddaneshmand79@gmail.com>
2025-11-01 19:12:45 -0400

    find minimum in rotated array

--- leetcode.py
+++ leetcode.py
@@ -1,18 +1,3 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        l, r = 0, len(nums) - 1
-        min_n = nums[0]
-
-        while l <= r:
-            if nums[l] < nums[r]:
-                min_n = min(nums[l], min_n)
-                return min_n
-
-            mid = (l + r) // 2
-            min_n = min(min_n, nums[mid])       
-             
-            if nums[mid] >= nums[l]:
-                l = mid + 1
-            else:
-                r = mid - 1
-        return min_n
+# review
