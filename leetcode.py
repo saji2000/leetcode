88d96769735d450fb7007a6941c457012750d6dc
saji2000 <sajaddaneshmand79@gmail.com>
2025-11-03 14:42:11 -0500

    Koko eating banana

--- leetcode.py
+++ leetcode.py
@@ -1,3 +1,22 @@
 class Solution:
-    def findMin(self, nums: List[int]) -> int:
-# review find min
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l, r = 0, max(piles)
+
+        k = r
+
+        while l <= r:
+            speed = (l + r) // 2
+            if speed == 0:
+                break
+                
+            time = 0
+
+            for i in piles:
+                time += math.ceil(i / speed)
+            if time <= h:
+                k = min(k, speed)
+                r = speed - 1
+            else:
+                l = speed + 1
+        return k
+
