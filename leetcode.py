7d2517e2bd168d4b88a2cc85485aba0c84c8a30d
saji2000 <sajaddaneshmand79@gmail.com>
2025-11-04 16:05:56 -0500

    min in rotated array

--- leetcode.py
+++ leetcode.py
@@ -1,17 +1,19 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         l, r = 0, len(nums) - 1
+
         minimum = nums[l]
 
         while l <= r:
             if nums[l] < nums[r]:
                 return min(minimum, nums[l])
-
+            
             mid = (l + r) // 2
             minimum = min(minimum, nums[mid])
 
-            if nums[l] <= nums[mid]:
+            if nums[mid] >= nums[l]:
                 l = mid + 1
             else:
                 r = mid - 1
+        
         return minimum
