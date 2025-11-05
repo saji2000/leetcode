f0353054ab9494caf8991f0b4a248696b2ee321f
saji2000 <sajaddaneshmand79@gmail.com>
2025-11-04 20:33:45 -0500

    search in rotated array

--- leetcode.py
+++ leetcode.py
@@ -1,19 +1,22 @@
 class Solution:
-    def findMin(self, nums: List[int]) -> int:
+    def search(self, nums: List[int], target: int) -> int:
         l, r = 0, len(nums) - 1
 
-        minimum = nums[l]
-
         while l <= r:
-            if nums[l] < nums[r]:
-                return min(minimum, nums[l])
-            
             mid = (l + r) // 2
-            minimum = min(minimum, nums[mid])
 
-            if nums[mid] >= nums[l]:
-                l = mid + 1
+            if nums[mid] == target:
+                return mid
+            
+            if nums[l] <= nums[mid]:
+                if target < nums[mid] and target >= nums[l]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
             else:
-                r = mid - 1
+                if target <= nums[r] and target > nums[mid]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
         
-        return minimum
+        return -1
