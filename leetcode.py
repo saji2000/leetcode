00b153e4f497284409a0151be1d7a899fdfbe909
saji2000 <sajaddaneshmand79@gmail.com>
2025-11-05 14:35:29 -0500

    Search in rotated array

--- leetcode.py
+++ leetcode.py
@@ -7,16 +7,17 @@ class Solution:
 
             if nums[mid] == target:
                 return mid
-            
+
             if nums[l] <= nums[mid]:
-                if target < nums[mid] and target >= nums[l]:
+                if target >= nums[l] and target < nums[mid]:
                     r = mid - 1
                 else:
                     l = mid + 1
+            
             else:
                 if target <= nums[r] and target > nums[mid]:
                     l = mid + 1
                 else:
                     r = mid - 1
-        
+
         return -1
