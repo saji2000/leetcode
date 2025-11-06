8b1195dd56976b246cba51591c45a6a14404ab89
saji2000 <sajaddaneshmand79@gmail.com>
2025-11-06 16:41:02 -0500

    Max profit

--- leetcode.py
+++ leetcode.py
@@ -1,23 +1,14 @@
-class TimeMap:
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l, r = 0, 1
+        maxProfit = 0
 
-    def __init__(self):
-        self.dict = defaultdict(list)
-
-    def set(self, key: str, value: str, timestamp: int) -> None:
-        self.dict[key].append([value, timestamp])
-
-    def get(self, key: str, timestamp: int) -> str:
-        values = self.dict[key]
-        l, r = 0, len(values) - 1
-        res = ""
-
-        while l <= r:
-            mid = (l + r) // 2
-
-            if values[mid][1] <= timestamp:
-                res = values[mid][0]
-                l = mid + 1
+        while r < len(prices):
+            
+            if prices[l] <= prices[r]:
+                profit = prices[r] - prices[l]
+                maxProfit = max(profit, maxProfit)
             else:
-                r = mid - 1
-        return res
-
+                l = r
+            r += 1
+        return maxProfit
