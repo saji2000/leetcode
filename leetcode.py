d0c01950c9e83e5a12112ef13a769959f18b7414
saji2000 <sajaddaneshmand79@gmail.com>
2025-11-08 22:35:21 -0500

    Longest character replacement

--- leetcode.py
+++ leetcode.py
@@ -1,19 +1,17 @@
 class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        l, r = 0, 0
+    def characterReplacement(self, s: str, k: int) -> int:
+        l = 0
         longest = 0
-        my_set = set()
-        string = ""
+        count = {}
+        max_count = 0
 
-        while r < len(s):
-            while s[r] in my_set:
-                my_set.remove(s[l])
+        for r in range(len(s)):
+            count[s[r]] = count.get(s[r], 0) + 1
+            max_count = max(max_count, count[s[r]])
+
+            while (r - l + 1) - max_count > k:
+                count[s[l]] -= 1
                 l += 1
-            
-            if s[r] not in my_set:
-                my_set.add(s[r])
-                longest = max(longest, len(my_set))
-                r += 1
+            longest = max(longest, r - l + 1) 
 
         return longest
-        
