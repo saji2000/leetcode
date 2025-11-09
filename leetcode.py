9506758411fef57634286a0be1a94df3f340fba8
saji2000 <sajaddaneshmand79@gmail.com>
2025-11-09 15:01:35 -0500

    Reverse Linked list

--- leetcode.py
+++ leetcode.py
@@ -1,18 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def characterReplacement(self, s: str, k: int) -> int:
-        l = 0
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        node = head
 
-        counts = {}
-        longest = 0
-        max_count = 0
-
-        for r in range(len(s)):
-            counts[s[r]] = counts.get(s[r], 0) + 1
-            max_count = max(counts.values())
-
-            while (r - l + 1) - max_count > k:
-                counts[s[l]] -= 1
-                l += 1
-
-            longest = max(longest, (r - l + 1))
-        return longest
+        while node:
+            nextNode = node.next
+            node.next = prev
+            prev = node
+            node = nextNode
+        
+        return prev
