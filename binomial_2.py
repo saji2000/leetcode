4f2706c0b8d11191ad8c8dd0f97467e0910e3514
saji2000 <sajaddaneshmand79@gmail.com>
2025-11-11 01:09:28 -0500

    cProfile

--- binomial_2.py
+++ binomial_2.py
@@ -1,26 +1,35 @@
 import math
+import cProfile
 
-n = 10  # Number of trials
-x = 6  # Number of successes
-p = 0.0526  # Probability of success
+def calculate_probability():
+    n = 1000000  # Number of trials
+    x = 20000  # Number of successes
+    p = 0.0526  # Probability of success
 
-# Calculate the binomial coefficient (nCx)
-nCx = math.comb(n, x)
+    # Calculate the binomial coefficient (nCx)
+    nCx = math.comb(n, x)
 
-# Calculate the probability using the binomial formula
-probability = nCx * (p**x) * ((1 - p) ** (n - x))
+    # Calculate the probability using the binomial formula
+    probability = nCx * (p**x) * ((1 - p) ** (n - x))
 
-formatted_probability = "{:.20f}".format(probability * 100)
+    formatted_probability = "{:.20f}".format(probability * 100)
 
-# Print the result
-print("Probability: " + formatted_probability + "%")
+    # Print the result
+    print("Probability: " + formatted_probability + "%")
 
-print("1 in :" + str(round(1 / probability)))
+    print("1 in :" + str(round(1 / probability)))
 
-log = math.log(1 / probability, 2)
+    log = math.log(1 / probability, 2)
 
-print("Similar to tossing a coin", round(log), "times and getting only heads")
+    print("Similar to tossing a coin", round(log), "times and getting only heads")
 
-log = math.log(1 / probability, 6)
+    log = math.log(1 / probability, 6)
 
-print("Similar to tossing a dice", round(log), "times and getting only 6")
+    print("Similar to tossing a dice", round(log), "times and getting only 6")
+
+# Profile only the function
+prof = cProfile.Profile()
+prof.enable()
+calculate_probability()
+prof.disable()
+prof.print_stats()
