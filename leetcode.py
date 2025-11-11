ca9416be5464feedf30922985122cedda911bb43
saji2000 <sajaddaneshmand79@gmail.com>
2025-11-10 19:17:55 -0500

    Merge two lists

--- leetcode.py
+++ leetcode.py
@@ -4,14 +4,27 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prev = None
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        head = ListNode(0)
         node = head
 
-        while node:
-            nextNode = node.next
-            node.next = prev
-            prev = node
-            node = nextNode
+        while list1 and list2:
+            if list1.val < list2.val:
+                node.next = list1
+                list1 = list1.next
+            else:
+                node.next = list2
+                list2 = list2.next
+            
+            node = node.next
+
+
+        if not list1 and list2:
+            node.next = list2
+        elif not list2 and list1:
+            node.next = list1
         
-        return prev
+        return head.next
+
+
+        
