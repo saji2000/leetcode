5f1769f929f2095c9aa710aae60d86308e06fd59
saji2000 <sajaddaneshmand79@gmail.com>
2025-11-12 14:37:37 -0500

    Cycle in linked list

--- leetcode.py
+++ leetcode.py
@@ -1,27 +1,20 @@
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        head = ListNode(0)
-        node = head
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if not head or not head.next:
+            return False
+            
+        slow = head
+        fast = head.next
 
-        while list1 and list2:
-            if list1.val < list2.val:
-                node.next = list1
-                list1 = list1.next
-            else:
-                node.next = list2
-                list2 = list2.next
-            node = node.next
-
-        if list1 and not list2:
-            node.next = list1
-
-        if list2 and not list1:
-            node.next = list2
-        
-        return head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
