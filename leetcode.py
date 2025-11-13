151999e54fd9babcbd730e6520e9c4f63357506e
saji2000 <sajaddaneshmand79@gmail.com>
2025-11-13 15:48:59 -0500

    Reorder list

--- leetcode.py
+++ leetcode.py
@@ -1,20 +1,37 @@
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def hasCycle(self, head: Optional[ListNode]) -> bool:
-        if not head or not head.next:
-            return False
-            
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
         slow = head
         fast = head.next
 
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-            if slow == fast:
-                return True
-        return False
+
+        second = slow.next
+        slow.next = None
+        prev = None
+
+        while second:
+            tmp = second.next
+            second.next = prev
+            prev = second
+            second = tmp
+        
+        first, second = head, prev
+
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first = tmp1
+            second = tmp2
+
+        return head
