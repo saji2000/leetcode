7dafebde33c8af287519b5d4da4a5d50e8cc1933
saji2000 <sajaddaneshmand79@gmail.com>
2025-11-19 16:55:55 -0500

    Find duplicate constant space

--- leetcode.py
+++ leetcode.py
@@ -1,42 +1,7 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        head = ListNode(0)
-        ans = head
-        carry = 0
-
-        while l1 and l2:
-            num = l1.val + l2.val + carry
-            carry = num // 10
-            num = num % 10
-
-            ans.next = ListNode(num)
-            ans = ans.next
-            l1 = l1.next
-            l2 = l2.next
-        
-        while l1:
-            num = l1.val + carry
-            carry = num // 10
-            num = num % 10
-            ans.next = ListNode(num)
-            ans = ans.next
-            l1 = l1.next
-
-        while l2:
-            num = l2.val + carry
-            carry = num // 10
-            num = num % 10
-            ans.next = ListNode(num)
-            ans = ans.next
-            l2 = l2.next
-        
-        if carry:
-            ans.next = ListNode(carry)
-            ans = ans.next
-        
-        return head.next
+    def findDuplicate(self, nums: List[int]) -> int:
+        nums.sort()
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i + 1]:
+                return nums[i]
+        return
