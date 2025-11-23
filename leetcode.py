69ecdad67acd76e4c3bf5d3264b5aef8e5238242
saji2000 <sajaddaneshmand79@gmail.com>
2025-11-22 21:50:08 -0500

    Find duplicate constant space

--- leetcode.py
+++ leetcode.py
@@ -1,6 +1,7 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        slow, fast = 0, 0
+        slow = 0
+        fast = 0
 
         while True:
             slow = nums[slow]
@@ -12,5 +13,6 @@ class Solution:
         while True:
             slow = nums[slow]
             slow2 = nums[slow2]
+
             if slow == slow2:
                 return slow
