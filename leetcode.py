bca811ca9e7cd322c08c23cbb2f94b5019121b05
saji2000 <sajaddaneshmand79@gmail.com>
2026-01-24 19:46:23 -0500

    LRU cache not finished yet

--- leetcode.py
+++ leetcode.py
@@ -1,17 +1,47 @@
-class Solution:
-    # duplicate
-    def findDuplicate(self, nums: List[int]) -> int:
-        slow = 0
-        fast = 0
+class ListNode:
+    def __init__(self, key = 0, val = 0, next = None, prev = None):
+        self.key = key
+        self.val = val
+        self.next = next
+        self.prev = prev
 
-        while True:
-            slow = nums[slow]
-            fast = nums[nums[fast]]
-            if fast == slow:
-                break
-        slow2 = 0
-        while True:
-            slow = nums[slow]
-            slow2 = nums[slow2]
-            if slow == slow2:
-                return slow
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.head = ListNode()
+        self.dict = {}
+    
+    def remove(self, key):
+        node = self.dict[key]
+        prev = node.prev
+        next = node.next
+        prev.next = next
+        next.prev = prev
+        del self.dict[key]
+    
+    def insert(self, key, val):
+        self.dict[key] = ListNode(key, val, self.head.next, self.head)
+        self.dict[key] = self.head.next.prev
+        self.head.next = self.dict[key]
+
+
+    def get(self, key: int) -> int:
+        if key in self.dict:
+            self.remove(key)
+            self.put
+            return self.dict[key]
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.dict:
+            self.remove(key)
+            self.put(key, value)
+            return
+        
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
