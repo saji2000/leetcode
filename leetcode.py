4e63a62c72581e73a68403de42b8959829765285
saji2000 <sajaddaneshmand79@gmail.com>
2026-01-31 13:02:09 -0500

    Diameter of Binary Tree

--- leetcode.py
+++ leetcode.py
@@ -8,7 +8,6 @@ class Solution:
     def __init__(self):
         self.diameter = 0
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-
         def height(node):
             if not node:
                 return 0
@@ -19,8 +18,7 @@ class Solution:
             self.diameter = max(self.diameter, left + right)
 
             return max(left, right) + 1
-        
+
         height(root)
 
         return self.diameter
-        
