7b994a4eead8a45477f950a22c93932756ea7c2c
saji2000 <sajaddaneshmand79@gmail.com>
2026-02-06 18:59:57 -0500

    Lowest common ancestor

--- leetcode.py
+++ leetcode.py
@@ -1,19 +1,21 @@
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 
 class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        node = root
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
 
+        node = root
         while node:
-            if p.val > node.val and q.val > node.val:
-                node = node.right
-            elif p.val < node.val and q.val < node.val:
+            if p.val < node.val and q.val < node.val:
                 node = node.left
+            elif p.val > node.val and q.val > node.val:
+                node = node.right
             else:
                 return node
+        
+
         
