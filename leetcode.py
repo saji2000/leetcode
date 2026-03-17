860343949b7433f1a836be45bec33a85b14d7a43
saji2000 <sajaddaneshmand79@gmail.com>
2026-03-17 11:56:57 -0400

    Build tree finished

--- leetcode.py
+++ leetcode.py
@@ -4,23 +4,15 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
 class Solution:
-    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        self.k = k
-        self.result = None
-        def traverse(node):
-            if not node or self.result is not None:
-                return
-            
-            traverse(node.left)
-
-            self.k -= 1
-            if self.k == 0:
-                self.result = node.val
-                return 
-            
-            traverse(node.right)
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        if not preorder or not inorder:
+            return None
         
-        traverse(root)
-        return self.result
+        root = TreeNode(preorder[0])
+        mid = inorder.index(preorder[0])
+
+        root.left = self.buildTree(preorder[1:mid + 1], inorder[:mid])
+        root.right = self.buildTree(preorder[mid + 1:], inorder[mid + 1:])
+
+        return root
