bdde9f71585aae76705a2742fdbc61413a3755ab
saji2000 <sajaddaneshmand79@gmail.com>
2026-03-18 10:13:04 -0400

    Build Tree finished, praise God

--- leetcode.py
+++ leetcode.py
@@ -7,12 +7,10 @@
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
         if not preorder or not inorder:
-            return None
+            return
         
         root = TreeNode(preorder[0])
-        mid = inorder.index(preorder[0])
-
-        root.left = self.buildTree(preorder[1:mid + 1], inorder[:mid])
-        root.right = self.buildTree(preorder[mid + 1:], inorder[mid + 1:])
-
+        index = inorder.index(preorder[0])
+        root.left = self.buildTree(preorder[1:index + 1], inorder[:index])
+        root.right = self.buildTree(preorder[index + 1:], inorder[index + 1:])
         return root
