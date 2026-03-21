874c92680cee6c222e327e2f75b71f8b10f91d82
saji2000 <sajaddaneshmand79@gmail.com>
2026-03-21 11:18:57 -0400

    Word dictionary, praise God

--- leetcode.py
+++ leetcode.py
@@ -1,34 +1,43 @@
 class TrieNode:
     def __init__(self):
         self.children = {}
-        self.endOfWord = False
+        self.is_end = False
 
-class PrefixTree:
+class WordDictionary:
 
     def __init__(self):
         self.root = TrieNode()
 
-    def insert(self, word: str) -> None:
+    def addWord(self, word: str) -> None:
         cur = self.root
         for c in word:
             if c not in cur.children:
                 cur.children[c] = TrieNode()
             cur = cur.children[c]
-        cur.endOfWord = True
-
+        cur.is_end = True
     def search(self, word: str) -> bool:
-        cur = self.root
-        for c in word:
-            if c not in cur.children:
-                return False
-            cur = cur.children[c]
-        return cur.endOfWord
-
-    def startsWith(self, prefix: str) -> bool:
-        cur = self.root
-        for c in prefix:
-            if c not in cur.children:
-                return False
-            cur = cur.children[c]
-        return True
+        return self._search(word, 0, self.root)
+    
+    def _search(self, word, index, node):
+        if index >= len(word):
+            return node.is_end
         
+        c = word[index]
+
+        if c == '.':
+            for child in node.children.values():
+                if self._search(word, index + 1, child):
+                    return True
+            return False
+        
+        else:
+            if c not in node.children:
+                return False
+            return self._search(word, index + 1, node.children[c])
+
+            
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
