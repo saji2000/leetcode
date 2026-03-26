035dfa9066e49d3cc7c18a3a31e4858f4a50d200
saji2000 <sajaddaneshmand79@gmail.com>
2026-03-26 11:45:09 -0400

    Combination sum review, praise God

--- leetcode.py
+++ leetcode.py
@@ -1,19 +1,18 @@
 class Solution:
-    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
         result = []
 
         def backtrack(index, cur, total):
             if sum(cur) == target:
                 result.append(cur.copy())
                 return
-            if index == len(candidates) or sum(cur) > target:
+            if index >= len(nums) or total > target:
                 return
-
-            cur.append(candidates[index])
-            backtrack(index, cur, total + candidates[index])
+            cur.append(nums[index])
+            backtrack(index, cur, total + nums[index])
             cur.pop()
 
             backtrack(index + 1, cur, total)
-
+        
         backtrack(0, [], 0)
         return result
