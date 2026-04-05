10c795665974efb9afbeaced5f5e8a5a480efff6
saji2000 <sajaddaneshmand79@gmail.com>
2026-04-04 21:20:39 -0400

    Parantheses, praise God

--- leetcode.py
+++ leetcode.py
@@ -1,21 +1,16 @@
 class Solution:
-    def subsetII(self, nums: List[int]) -> List[List[int]]:
+    def generateParenthesis(self, n: int) -> List[str]:
         result = []
-        cur = []
-        nums.sort()
 
-        def backtrack(index):
-            if index == len(nums):
-                result.append(cur.copy())
+        def backtrack(opening, closing, cur):
+            if len(cur) == 2 * n:
+                result.append(cur)
                 return
-            
-            cur.append(nums[index])
-            backtrack(index + 1)
-            cur.pop()
-            
-            while index < len(nums) + 1 and nums[index] == nums[index + 1]:
-                index += 1
-            
-            backtrack(index + 1)
-        backtrack([])
+            if opening < n:
+                backtrack(opening + 1, closing, cur + '(')
+                
+            if closing < opening:
+                backtrack(opening, closing + 1, cur + ')')
+        
+        backtrack(0, 0, "")
         return result
