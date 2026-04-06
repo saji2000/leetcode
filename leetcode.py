96a97cab449ad8d5f6a967af9fea042780a505f3
saji2000 <sajaddaneshmand79@gmail.com>
2026-04-06 13:47:05 -0400

    generate parantheses reviewed, praise God

--- leetcode.py
+++ leetcode.py
@@ -2,15 +2,17 @@ class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         result = []
 
-        def backtrack(opening, closing, cur):
+        def backtrack(closing, opening, cur):
             if len(cur) == 2 * n:
                 result.append(cur)
                 return
+            
             if opening < n:
-                backtrack(opening + 1, closing, cur + '(')
-                
+                backtrack(closing, opening + 1, cur + "(")
+            
             if closing < opening:
-                backtrack(opening, closing + 1, cur + ')')
+                backtrack(closing + 1, opening, cur + ")")
         
         backtrack(0, 0, "")
         return result
+            
