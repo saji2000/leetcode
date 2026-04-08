96f1ee87d7031bef8dba3184e4ec55b79159abf9
saji2000 <sajaddaneshmand79@gmail.com>
2026-04-08 17:27:23 -0400

    word search

--- leetcode.py
+++ leetcode.py
@@ -1,18 +1,23 @@
 class Solution:
-    def generateParenthesis(self, n: int) -> List[str]:
-        result = []
-
-        def backtrack(closing, opening, cur):
-            if len(cur) == 2 * n:
-                result.append(cur)
-                return
-            
-            if opening < n:
-                backtrack(closing, opening + 1, cur + "(")
-            
-            if closing < opening:
-                backtrack(closing + 1, opening, cur + ")")
+    def exist(self, board: List[List[str]], word: str) -> bool:
         
-        backtrack(0, 0, "")
-        return result
+        def backtrack(i, j, k):
+            if k == len(word):
+                return True
             
+            if i < 0 or j < 0 or i >= len(board[0]) or j >= len(board) or board[j][i] != word[k]:
+                return False
+            
+            temp = board[j][i]
+            board[j][i] = ''
+            if backtrack(i + 1, j, k + 1) or backtrack(i - 1, j, k + 1) or backtrack(i, j + 1, k + 1) or backtrack(i, j - 1, k + 1):
+                return True
+            board[j][i] = temp
+            return False
+        
+
+        for j in range(len(board)):
+            for i in range(len(board[0])):
+                if backtrack(i, j, 0):
+                    return True
+        return False
