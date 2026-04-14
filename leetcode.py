35e995fc60e9d6f965088e3ed4d6d605a084b71b
saji2000 <sajaddaneshmand79@gmail.com>
2026-04-14 10:57:30 -0400

    Partition of palindromes

--- leetcode.py
+++ leetcode.py
@@ -1,23 +1,17 @@
 class Solution:
-    def exist(self, board: List[List[str]], word: str) -> bool:
-        
-        def backtrack(i, j, k):
-            if k == len(word):
-                return True
-            
-            if i < 0 or j < 0 or i >= len(board[0]) or j >= len(board) or board[j][i] != word[k]:
-                return False
-            
-            temp = board[j][i]
-            board[j][i] = ''
-            if backtrack(i + 1, j, k + 1) or backtrack(i - 1, j, k + 1) or backtrack(i, j + 1, k + 1) or backtrack(i, j - 1, k + 1):
-                return True
-            board[j][i] = temp
-            return False
-        
+    def partition(self, s: str) -> List[List[str]]:
+        result = []
 
-        for j in range(len(board)):
-            for i in range(len(board[0])):
-                if backtrack(i, j, 0):
-                    return True
-        return False
+        def backtrack(start, substr):
+            if start >= len(s):
+                result.append(substr.copy())
+            
+            for end in range(start, len(s)):
+                if s[start:end + 1] == s[start:end + 1][::-1]:
+                    substr.append(s[start:end + 1])
+                    backtrack(end + 1, substr)
+                    substr.pop()
+        
+        backtrack(0, [])
+        
+        return result
