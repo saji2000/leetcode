4704df2892a945ac884aa40b0555f38fa26ea47a
saji2000 <sajaddaneshmand79@gmail.com>
2026-04-21 13:22:14 -0400

    Letter Combinations

--- leetcode.py
+++ leetcode.py
@@ -1,20 +1,17 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
         result = []
         nums = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
-
-        def backtrack(i, comb):
-            if len(comb) == len(digits):
-                result.append(comb[:])
+        
+        def backtrack(i, cur):
+            if len(cur) == len(digits):
+                result.append(cur[:])
                 return
             
             for letter in nums[digits[i]]:
-                print(i)
-                print(comb + letter)
-                backtrack(i + 1, comb + letter)
+                backtrack(i + 1, cur + letter)
         
         backtrack(0, "")
         return result
-
-solution = Solution()
-print(solution.letterCombinations("234"))
